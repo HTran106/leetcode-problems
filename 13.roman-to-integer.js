ff85c87b0e835be0c564d87b5d55ed97b1206e8b
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-10-12 15:41:22 -0700

    romanToInt

--- 13.roman-to-integer.js
+++ 13.roman-to-integer.js
@@ -10,7 +10,23 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    const romanMap = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
+    let result = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (romanMap[s[i]] < romanMap[s[i + 1]]) {
+            result -= romanMap[s[i]];
+        } else {
+            result += romanMap[s[i]];
+        }
+    }
+    return result;
 };
 // @lc code=end
-
