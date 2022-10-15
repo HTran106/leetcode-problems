a5d68ebdef6f4fbee6c07a62ea63b85670dc1db1
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-10-15 09:30:08 -0700

    done with removeElement

--- 27.remove-element.js
+++ 27.remove-element.js
@@ -10,12 +10,14 @@
  * @param {number} val
  * @return {number}
  */
-var removeElement = function(nums, val) {
-    nums.forEach(num => {
-        if (num === val) {
-            nums.splice(nums.indexOf(num), 1, "_");
+var removeElement = function (nums, val) {
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[count] = nums[i];
+            count++;
         }
-    })
-    return nums
+    }
+    return count;
 };
 // @lc code=end
