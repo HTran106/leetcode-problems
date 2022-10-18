d6c6c4c8845315c0eb4c90ac96cf259f7dae73f2
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-10-18 15:50:33 -0700

    done with merge2arrays

--- 88.merge-sorted-array.js
+++ 88.merge-sorted-array.js
@@ -13,12 +13,25 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function (nums1, m, nums2, n) {
-    nums1 = nums1.filter((num, i) => i < m)
-    nums2 = nums2.filter((num, i) => i < n)
-    nums1.push(...nums2)
-    nums1 = nums1.sort((a, b) => a - b)
-    console.log(nums1)
+    let i = m - 1;
+    let j = n - 1;
+    let k = m + n - 1;
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+        nums1[k] = nums1[i];
+        i--;
+        } else {
+        nums1[k] = nums2[j];
+        j--;
+        }
+        k--;
+    }
+    while (j >= 0) {
+        nums1[k] = nums2[j];
+        j--;
+        k--;
+    }
 };
 
-merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
+
 // @lc code=end
