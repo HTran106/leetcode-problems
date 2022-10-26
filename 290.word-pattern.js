07112956739decbf39b3eef0cb5532d1905b139d
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-10-25 21:29:04 -0700

    still working on word pattern

---
+++ 290.word-pattern.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=290 lang=javascript
+ *
+ * [290] Word Pattern
+ */
+
+// @lc code=start
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function (pattern, s) {
+    const words = new Set(s.split(' '))
+    const [word1, word2] = words
+
+    for (let i = 0; i < pattern.length; i++) {
+        let patt = pattern[i]
+        for (let j = 0; j < s.split(' '.length); j++) {
+            let w = s.split(' ')[i]
+            if (w !== word1 && w !== word2) return false
+            if (patt === 'a' && w !== word1) return false
+            if (patt === 'b' && w !== word2) return false
+        }
+    }
+    return true
+};
+// @lc code=end
