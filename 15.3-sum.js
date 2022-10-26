fe6628132bd8048cebefb6c98e2f692fada4c510
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-10-26 11:52:18 -0700

    working on 3sum

---
+++ 15.3-sum.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=15 lang=javascript
+ *
+ * [15] 3Sum
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+    const res = []
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            for (let k = j + 1; k < nums.length; k++) {
+                if (nums[i] + nums[j] + nums[k] === 0) {
+                    if (!res.includes([nums[i], nums[j], nums[k]])) {
+                        res.push([nums[i], nums[j], nums[k]])
+                    }
+                }
+            }
+        }
+    }
+    return res
+};
+// @lc code=end
---
+++ 404.sum-of-left-leaves.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=404 lang=javascript
+ *
+ * [404] Sum of Left Leaves
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function(root) {
+    let rootNode = root.val
+    let sum = 0
+
+    let currNode = rootNode.shift()
+    while (currNode.left) {
+        sum += currNode.val
+        currNode = currNode.left
+    }
+    return sum
+};
+
+
+// @lc code=end
