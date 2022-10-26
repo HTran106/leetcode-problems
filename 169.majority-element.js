701d22cc019b7407d4f879b5c20ae57521757ff2
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-10-25 21:17:38 -0700

    done with majority element

--- 169.majority-element.js
+++ 169.majority-element.js
@@ -9,7 +9,7 @@
  * @param {number[]} nums
  * @return {number}
  */
-var majorityElement = function(nums) {
+var majorityElement = function (nums) {
     const res = {};
     nums.forEach(num => {
         if (res[num]) {
@@ -18,12 +18,13 @@ var majorityElement = function(nums) {
             res[num] = 1
         }
     })
-    console.log(res)
+
     const max = Math.max(...Object.values(res))
+
     for (let num in res) {
         if (res[num] === max) return num
     }
 };
 
-majorityElement([3,2,3])
+
 // @lc code=end
