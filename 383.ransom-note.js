e734c3eab5ad4de76c4828dd6714a55f2c64dd1d
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-11-06 14:32:53 -0800

    done with ranson note

---
+++ 383.ransom-note.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=383 lang=javascript
+ *
+ * [383] Ransom Note
+ */
+
+// @lc code=start
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+    return magazine.length >= ransomNote.length && [...ransomNote].every(c => {
+        let i = magazine.indexOf(c);
+        if (i >= 0) {
+            magazine = magazine.slice(0, i) + magazine.slice(i + 1);
+            return true;
+        }
+        return false;
+    }
+    );
+};
+// @lc code=end
