713cf3ae6f2d9b2820cae8b87d666e11c0432cf3
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-11-11 10:08:13 -0800

    working on minDepth

---
+++ 111.minimum-depth-of-binary-tree.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=111 lang=javascript
+ *
+ * [111] Minimum Depth of Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+    let queue = [root];
+    let depth = 0;
+    
+};
+// @lc code=end
