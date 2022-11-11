7ea23ab6a0bf2a2f2c78d5bbf6e199142da033d1
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-11-10 18:10:11 -0800

    working on getIntersection

---
+++ 160.intersection-of-two-linked-lists.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=160 lang=javascript
+ *
+ * [160] Intersection of Two Linked Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    let curr1 = headA;
+    let curr2 = headB;
+
+    while (curr1 || curr2) {
+        
+    }
+};
+// @lc code=end
