8c37389e3868857f82e2e4f418cb7faea0656723
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-11-14 12:13:31 -0800

    done with reverse num

--- 7.reverse-integer.js
+++ 7.reverse-integer.js
@@ -10,10 +10,14 @@
  * @return {number}
  */
 var reverse = function (x) {
-    let res = ''
-    if (x < 0) {
-        res += '-'
-        res += +
+    let num = x.toString().split('').reverse().join('')
+    if (num[num.length - 1] === '-') {
+        num = num.slice(0, num.length - 1)
+        num = '-' + num
     }
+    num = +num
+    if (num > 2 ** 31 - 1 || num < -(2 ** 31)) return 0
+    return num
 };
+
 // @lc code=end
