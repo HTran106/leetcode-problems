ccae1fc64db36fabd2daedaf1d6321e17c01f456
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-11-15 16:44:34 -0800

    working on arranging coins

---
+++ 441.arranging-coins.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=441 lang=javascript
+ *
+ * [441] Arranging Coins
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var arrangeCoins = function(n) {
+    let arr = [];
+    for (let i = 1; i < n; i++) {
+        arr.push(new Array(i))
+        n -= i
+    }
+    return arr.length
+};
+// @lc code=end
