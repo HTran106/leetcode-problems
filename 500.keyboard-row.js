4e680a2be081298e9b81619787c9cdf649e57ae7
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-11-19 09:28:54 -0800

    keyboard row not done

--- 500.keyboard-row.js
+++ 500.keyboard-row.js
@@ -15,6 +15,19 @@ var findWords = function(words) {
     let bot = "zxcvbnm,./";
     let result = [];
     for (let i = 0; i < words.length; i++) {
-        
+        let word = words[i].toLowerCase();
+        for (let j = 0; j < word.length; j++) {
+            let char = word[j]
+            if (!top.includes(char)) {
+                if (!mid.includes(char)) {
+                    if (!bot.includes(char)) {
+                        break
+                    }
+                }
+            }
+        }
+        result.push(word)
+    }
+    return result
 };
 // @lc code=end
