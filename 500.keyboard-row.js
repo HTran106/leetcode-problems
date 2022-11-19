8f32170e6a05469eae6be40d58725ad4124746df
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-11-19 09:23:59 -0800

    working on find words

---
+++ 500.keyboard-row.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=500 lang=javascript
+ *
+ * [500] Keyboard Row
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var findWords = function(words) {
+    let top = "qwertyuiop[]";
+    let mid = "asdfghjkl;'";
+    let bot = "zxcvbnm,./";
+    let result = [];
+    for (let i = 0; i < words.length; i++) {
+        
+};
+// @lc code=end
