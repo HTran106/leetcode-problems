3791fed8c815c77b6be65fdc903abef733f779e8
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-11-23 08:49:34 -0800

    sum root to leaf not working

---
+++ 1022.sum-of-root-to-leaf-binary-numbers.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=1022 lang=javascript
+ *
+ * [1022] Sum of Root To Leaf Binary Numbers
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumRootToLeaf = function(root) {
+    if (!root) return 0
+    if (!root.left && !root.right) return root.val.toString(16)
+    return sumRootToLeaf(root.left).toString(16) + sumRootToLeaf(root.right).toString(16)
+
+};
+// @lc code=end
