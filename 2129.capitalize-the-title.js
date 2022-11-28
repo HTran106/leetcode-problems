4a1a4d042258905d47fa4f7a4e55073b75bd9cbf
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-11-28 10:22:31 -0800

    still working on capitalizetitle

--- 2129.capitalize-the-title.js
+++ 2129.capitalize-the-title.js
@@ -10,10 +10,12 @@
  * @return {string}
  */
 var capitalizeTitle = function(title) {
-    title = title.split(' ')
-    title.map(word => {
+    let arr = title.split(' ')
+    arr.map(word => {
         if (word.length <= 2) return word.toLowerCase()
-        else return word[0].toUpperCase()
+        console.log(word[0].toUpperCase() + word.slice(1).toLowerCase())
     })
+    console.log(arr)
+    return arr.join(' ')
 };
 // @lc code=end
