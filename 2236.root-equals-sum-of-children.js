439dd356e77e12988a3fd506feab2592a7933a61
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-01 12:30:38 -0800

    done with checktree

---
+++ 2236.root-equals-sum-of-children.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=2236 lang=javascript
+ *
+ * [2236] Root Equals Sum of Children
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var checkTree = function(root) {
+    return root.left.val + root.right.val === root.val
+};
+// @lc code=end
