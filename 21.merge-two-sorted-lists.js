d89e97ce1453d608fc3234be6e1826e0700dd8b6
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-06 11:30:34 -0800

    redoing merge two sorted list

--- 21.merge-two-sorted-lists.js
+++ 21.merge-two-sorted-lists.js
@@ -18,7 +18,20 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function (list1, list2) {
-    
+    let dummyHead = new ListNode()
+    let curr = dummyHead
+    while (list1 && list2) {
+        if (list1.val < list2.val) {
+            curr.next = list1
+            list1 = list1.next
+        } else {
+            curr.next = list2
+            list2 = list2.next
+        }
+        curr = curr.next
+    }
+    curr.next = list1 || list2
+    return dummyHead.next
 };
 // @lc code=end
 
