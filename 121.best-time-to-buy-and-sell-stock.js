3b936441e4dbca1c0f69cc1eac237b1d2bd3f7c0
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-06 12:03:59 -0800

    working on #121

--- 121.best-time-to-buy-and-sell-stock.js
+++ 121.best-time-to-buy-and-sell-stock.js
@@ -9,8 +9,15 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
-    
+var maxProfit = function (prices) {
+    let max = 0
+    for (let i = 0; i < prices.length; i++) {
+        let curr = prices[i]
+        let maxFutures = Math.max(...prices.splice(i + 1))
+        let profit = maxFutures - curr
+        console.log(profit)
+        if (profit > max) max = profit
+    }
+    return max
 };
 // @lc code=end
-
