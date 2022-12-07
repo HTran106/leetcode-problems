46df2fa0cc256cdaaa7d0cefcc81d28a0e2f7002
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-07 13:22:34 -0800

    done with sumamryRanges

--- 228.summary-ranges.js
+++ 228.summary-ranges.js
@@ -10,7 +10,14 @@
  * @return {string[]}
  */
 var summaryRanges = function(nums) {
-    
+    for (let i = 0; i < nums.length; i++) {
+        let j = i;
+        while (nums[j] + 1 === nums[j + 1]) j++;
+        if (i < j) {
+            nums[i] = nums[i] + '->' + nums[j];
+            nums.splice(i + 1, j - i);
+        }
+    }
+    return nums;
 };
 // @lc code=end
-
