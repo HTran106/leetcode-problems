a94946457197354afdecdb29cd8c0456856675a7
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-07 13:20:34 -0800

    working on summaryRanges

---
+++ 228.summary-ranges.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=228 lang=javascript
+ *
+ * [228] Summary Ranges
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function(nums) {
+    
+};
+// @lc code=end
+
