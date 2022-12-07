b636f284a440f585d6822679495252c17697788d
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-06 16:04:39 -0800

    search 2d matrix done

---
+++ 74.search-a-2-d-matrix.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=74 lang=javascript
+ *
+ * [74] Search a 2D Matrix
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    if (!matrix.flat().includes(target)) return false
+    return true
+};
+// @lc code=end
