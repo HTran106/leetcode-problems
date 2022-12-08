686319ebb644489e4e813dc5ef9fca839282e2ad
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-08 13:25:48 -0800

    done with base 7

---
+++ 504.base-7.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=504 lang=javascript
+ *
+ * [504] Base 7
+ */
+
+// @lc code=start
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var convertToBase7 = function(num) {
+    return num.toString(7)
+};
+// @lc code=end
