e4acd7129aee9b55c4ad51f2d0a18bf179ede0c2
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-09 00:45:14 -0800

    working on pseudocoding

--- 15.3-sum.js
+++ 15.3-sum.js
@@ -10,33 +10,33 @@
  * @return {number[][]}
  */
 var threeSum = function (nums) {
-    let result = [];
-    let sortedNums = nums.sort((a, b) => a - b);
-    for (let i = 0; i < sortedNums.length; i++) {
-        if (i > 0 && sortedNums[i] === sortedNums[i - 1]) {
-            continue;
-        }
-        let left = i + 1;
-        let right = sortedNums.length - 1;
-        while (left < right) {
-            let sum = sortedNums[i] + sortedNums[left] + sortedNums[right];
-            if (sum === 0) {
-                result.push([sortedNums[i], sortedNums[left], sortedNums[right]]);
-                left++;
-                right--;
-                while (left < right && sortedNums[left] === sortedNums[left - 1]) {
-                    left++;
-                }
-                while (left < right && sortedNums[right] === sortedNums[right + 1]) {
-                    right--;
-                }
-            } else if (sum < 0) {
-                left++;
-            } else {
-                right--;
-            }
-        }
-    }
-    return result;
+    // let result = [];
+    // let sortedNums = nums.sort((a, b) => a - b);
+    // for (let i = 0; i < sortedNums.length; i++) {
+    //     if (i > 0 && sortedNums[i] === sortedNums[i - 1]) {
+    //         continue;
+    //     }
+    //     let left = i + 1;
+    //     let right = sortedNums.length - 1;
+    //     while (left < right) {
+    //         let sum = sortedNums[i] + sortedNums[left] + sortedNums[right];
+    //         if (sum === 0) {
+    //             result.push([sortedNums[i], sortedNums[left], sortedNums[right]]);
+    //             left++;
+    //             right--;
+    //             while (left < right && sortedNums[left] === sortedNums[left - 1]) {
+    //                 left++;
+    //             }
+    //             while (left < right && sortedNums[right] === sortedNums[right + 1]) {
+    //                 right--;
+    //             }
+    //         } else if (sum < 0) {
+    //             left++;
+    //         } else {
+    //             right--;
+    //         }
+    //     }
+    // }
+    // return result;
 };
 // @lc code=end
--- 9.palindrome-number.js
+++ 9.palindrome-number.js
@@ -9,7 +9,23 @@
  * @param {number} x
  * @return {boolean}
  */
+/*
+Input: integer
+Output: Boolean
+
+turn integer into a string
+turn string into an array
+reverse the array
+turn the array back into a string
+compare the original integer string to the reversed integer string
+
+*/
+
+
 var isPalindrome = function(x) {
-    return x.toString() === x.toString().split('').reverse().join('');
+    return x.toString() === x.toString()
+                            .split('')
+                            .reverse()
+                            .join('')
 };
 // @lc code=end
