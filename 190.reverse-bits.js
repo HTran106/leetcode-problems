e4738dbac69e74da53b740441baa3c60bde0d627
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-08 21:48:08 -0800

    working on reverseBits

---
+++ 190.reverse-bits.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=190 lang=javascript
+ *
+ * [190] Reverse Bits
+ */
+
+// @lc code=start
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+var reverseBits = function (n) {
+    
+};
+
+
+reverseBits(00000010100101000001111010011100)
+// @lc code=end
