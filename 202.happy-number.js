923e451a79721cce539ecc4e8627547bfb292ee0
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-13 09:37:56 -0800

    working on ishappy

---
+++ 202.happy-number.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=202 lang=javascript
+ *
+ * [202] Happy Number
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+
+
+/*
+input: number;
+output: boolean;
+
+approach:
+i would turn the number into a string, split it into an array.
+use reduce to square it and get a new number
+and keep repeating until it equals 1 or less
+break the loop if lesser than one and return rfalse
+
+*/
+var isHappy = function(n) {
+    let num = n.toString().split('')
+    while (true) {
+        
+    }
+};
+
+// @lc code=end
