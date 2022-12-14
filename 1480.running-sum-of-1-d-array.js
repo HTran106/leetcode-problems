96cf4e7e3b99a03626ca68240aa5e9aec2d4db21
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-14 08:38:51 -0800

    done with running sum

--- 1480.running-sum-of-1-d-array.js
+++ 1480.running-sum-of-1-d-array.js
@@ -10,6 +10,9 @@
  * @return {number[]}
  */
 var runningSum = function(nums) {
-    return nums.map(())
+    return nums.map((el, i) => {
+        return nums.slice(0, i + 1)
+                   .reduce((a, c) => a + c)
+    })
 };
 // @lc code=end
