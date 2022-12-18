98533983ffaec23e4288e8cdb2b0c3b6e8c0c59d
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-18 12:56:58 -0800

    working on thirdMax

---
+++ 414.third-maximum-number.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=414 lang=javascript
+ *
+ * [414] Third Maximum Number
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function(nums) {
+    
+};
+// @lc code=end
+
