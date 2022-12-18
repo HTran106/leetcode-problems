4fbb3dde1f94fa4fb0be50751097529d05b9474f
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-18 13:02:21 -0800

    done with thirdMax

--- 414.third-maximum-number.js
+++ 414.third-maximum-number.js
@@ -9,8 +9,11 @@
  * @param {number[]} nums
  * @return {number}
  */
-var thirdMax = function(nums) {
-    
+var thirdMax = function (nums) {
+    let unique = new Set(nums)
+    let sorted = Array.from(unique)
+    if (sorted.length <= 2) return Math.max(...sorted)
+    else sorted.sort((a, b) => a - b)
+    return sorted[sorted.length - 3]
 };
 // @lc code=end
-
