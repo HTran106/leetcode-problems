19836791286dc46a8f13def3f00220656ba87d51
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-19 13:06:44 -0800

    working on countSegments

---
+++ 434.number-of-segments-in-a-string.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=434 lang=javascript
+ *
+ * [434] Number of Segments in a String
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSegments = function (s) {
+    if (!s.length) return 0
+    return s.split(' ').length
+};
+// @lc code=end
