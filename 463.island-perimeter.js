6f5d567ded23e5c861d5c91b1db1d7fc849c5c9f
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-20 13:33:01 -0800

    working on islandPerimeter

--- 463.island-perimeter.js
+++ 463.island-perimeter.js
@@ -18,10 +18,15 @@
 // ]
 
 var islandPerimeter = function (grid) {
+    let count = 0
     for (let row = 0; row < grid.length; row++) {
         for (let col = 0; col < grid[row].length; col++) {
             if (grid[row][col] === 1) {
-                return dfs(grid, row, col);
+                if (row > 0 && grid[row + 1][col] !== 1) count++
+                if (row > 0 && grid[row - 1][col] !== 1) count++
+                if (col > 0 && grid[row][col - 1] !== 1) count++
+                if (col > 0 && grid[row][col +] !== 1) count++
+
             }
         }
     }
