4ca91ef384236c16d74ba8d721d3b13be2f29d4b
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-20 13:22:50 -0800

    working on islandPerimeter

---
+++ 463.island-perimeter.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=463 lang=javascript
+ *
+ * [463] Island Perimeter
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+
+
+var islandPerimeter = function(grid) {
+    let count = 0
+
+    for (let row = 0; row < grid.length; row++) {
+        for (let col = 0; col < grid[row].length; col++) {
+            if (grid[row][col] === 1) {
+                if (grid[row - 1][col] && grid[row - 1][col] !== 1) count++
+                if (grid[row + 1][col] && grid[row + 1][col] !== 1) count++
+                if (grid[row][col - 1] && grid[row][col - 1] !== 1) count++
+                if (grid[row][col + 1] && grid[row][col + 1] !== 1) count++
+            }
+        }
+    }
+    return count
+};
+// @lc code=end
