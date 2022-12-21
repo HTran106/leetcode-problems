b21f435a5c9893f66b0bd409ae8b915dc7604148
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-21 13:39:04 -0800

    working on relative ranks

---
+++ 506.relative-ranks.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=506 lang=javascript
+ *
+ * [506] Relative Ranks
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} score
+ * @return {string[]}
+ */
+var findRelativeRanks = function(score) {
+    
+};
+// @lc code=end
+
