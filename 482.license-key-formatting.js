156f09c27fceaf9e41b869bf42a1efbd3c0ee66b
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-22 09:25:25 -0800

    working on license key formatting

---
+++ 482.license-key-formatting.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=482 lang=javascript
+ *
+ * [482] License Key Formatting
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var licenseKeyFormatting = function(s, k) {
+    s = s.split("-")
+    let res = []
+    let counter = 0
+    let arr = []
+    for (let i = s.length - 1; i >= 0; i--) {
+        
+
+    }
+};
+// @lc code=end
