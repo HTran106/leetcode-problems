6cd87e47e2b6abfdd517c9746c408d49ea9e29e5
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-22 09:33:47 -0800

    working on license key formatting

--- 482.license-key-formatting.js
+++ 482.license-key-formatting.js
@@ -11,13 +11,21 @@
  * @return {string}
  */
 var licenseKeyFormatting = function(s, k) {
-    s = s.split("-")
+    let copy = s.replaceAll("-", "")
+    console.log(copy)
     let res = []
-    let counter = 0
+    let counter = 1
     let arr = []
     for (let i = s.length - 1; i >= 0; i--) {
-        
-
+        if (counter === k) {
+            res.unshift(str)
+            str = ''
+            counter = 1
+        } else {
+            str += copy[i]
+            counter++
+        }
     }
+    return res.join("-")
 };
 // @lc code=end
