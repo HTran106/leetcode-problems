66fec3b2a832da42ddd3301da9ed14788ccd6b38
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-23 09:10:51 -0800

    working on covert time

--- 2224.minimum-number-of-operations-to-convert-time.js
+++ 2224.minimum-number-of-operations-to-convert-time.js
@@ -13,5 +13,7 @@
 var convertTime = function(current, correct) {
     const currentHr = current.split(":")
     const correctHr = correct.split(':')
+    const times = correctHr[0] - currentHr[0]
+    return correctHr[1] - currentHr[1] === 0 ? times : times + 1
 };
 // @lc code=end
