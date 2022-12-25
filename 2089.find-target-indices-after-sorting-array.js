ff992bfa48069a057edfbb3a74cf7e026601caf5
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-25 09:37:22 -0800

    working on targetIndices

---
+++ 2089.find-target-indices-after-sorting-array.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=2089 lang=javascript
+ *
+ * [2089] Find Target Indices After Sorting Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var targetIndices = function(nums, target) {
+    
+};
+// @lc code=end
+
