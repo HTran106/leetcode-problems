386a28e2f232fb8629996ec5c336adbcb0a6a4f1
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-25 09:35:34 -0800

    done with countEven

--- 2180.count-integers-with-even-digit-sum.js
+++ 2180.count-integers-with-even-digit-sum.js
@@ -12,11 +12,11 @@
 var countEven = function (num) {
     let count = 0
     for (let i = 1; i <= num; i++) {
-        const n = i.toString().split('').reduce((a, c) => a + c)
+        const n = i.toString().split('').reduce((a, c) => parseInt(a) + parseInt(c))
         console.log(n)
         n % 2 === 0 ? count++ : null
     }
     return count
 };
 // @lc code=end
-console.log(parseInt(19).toString().split('').reduce((a, c) => parseInt(a) + parseInt(c)))
+// console.log(parseInt(19).toString().split('').reduce((a, c) => parseInt(a) + parseInt(c)))
