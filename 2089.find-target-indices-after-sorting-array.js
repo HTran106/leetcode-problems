b0d1f078461f33d25d4f602060d6b842b06d554f
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-25 09:38:29 -0800

    done with targetIndices

--- 2089.find-target-indices-after-sorting-array.js
+++ 2089.find-target-indices-after-sorting-array.js
@@ -11,7 +11,11 @@
  * @return {number[]}
  */
 var targetIndices = function(nums, target) {
-    
+    nums.sort((a, b) => a - b)
+    const indices = []
+    for (let i = 0; i < nums.length; i++) {
+        nums[i] === target ? indices.push(i) : null
+    }
+    return indices
 };
 // @lc code=end
-
