fab1d5cbcc1393374dcb28aecd4450ff0ca48070
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-28 11:52:54 -0800

    working on findWords

--- 500.keyboard-row.js
+++ 500.keyboard-row.js
@@ -24,14 +24,17 @@ var findWords = function (words) {
     for (let i = 0; i < words.length; i++) {
         row = 1
         let word = words[i]
-        for (let j = 0; j < word.length; j++) {
-            let char = word[j].toLowerCase()
-            if (!rows[row].includes(char)) {
-                j = 0
-                row++
+        if (word.length === 1) res.push(word)
+        else {
+            for (let j = 0; j < word.length; j++) {
+                let char = word[j].toLowerCase()
+                if (!rows[row].includes(char)) {
+                    j = 0
+                    row++
+                }
+                if (row === 4) break
+                if (j === word.length - 1 && rows[row].includes(char)) res.push(word)
             }
-            if (row === 4) break
-            if (j === word.length - 1 && rows[row].includes(char)) res.push(word)
         }
     }
     return res
