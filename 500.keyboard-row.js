3282a337a66173e7f97c337da0050c3225db2b9a
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-28 12:01:12 -0800

    working on findWords

--- 500.keyboard-row.js
+++ 500.keyboard-row.js
@@ -14,5 +14,36 @@ var findWords = function (words) {
     let mid = "asdfghjkl";
     let bot = "zxcvbnm";
 
+    let res = []
+    for (let i = 0; i < words.length; i++) {
+        let word = words[i].toLowerCase();
+        let row = 0;
+        for (let j = 0; j < word.length; j++) {
+            let char = word[j];
+            if (top.includes(char)) {
+                if (row == 0) {
+                    row = 1;
+                } else if (row == 2 || row == 3) {
+                    row = 0;
+                    break;
+                }
+            } else if (mid.includes(char)) {
+                if (row == 0) {
+                    row = 2;
+                } else if (row == 1 || row == 3) {
+                    row = 0;
+                    break;
+                }
+                
+    words.forEach(word => {
+        word.forEach(char => {
+            char = char.toLowerCase();
+            if (!top.includes(char) && !mid.includes(char) && !bot.includes(char)) {
+                return;
+            }
+        })
+        res.push(word);
+    })
+    return res;
 };
 // @lc code=end
