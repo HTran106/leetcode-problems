148fa73b1b97093e2fd982d6d59f9d792206b71c
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-30 12:11:22 -0800

    working on validPalindrome2

---
+++ 680.valid-palindrome-ii.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=680 lang=javascript
+ *
+ * [680] Valid Palindrome II
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function(s) {
+    for (let i = 0; i < s.length; i++) {
+        
+    }
+};
+// @lc code=end
