8f7adeef20b86a07a198d5d1e995d887e5a37ee5
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-12-30 12:13:32 -0800

    working on validPalindrome2

--- 680.valid-palindrome-ii.js
+++ 680.valid-palindrome-ii.js
@@ -9,9 +9,28 @@
  * @param {string} s
  * @return {boolean}
  */
+function isPalindrome(s) {
+    let left = 0;
+    let right = s.length - 1;
+    while (left < right) {
+        if (s[left] != s[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
+
 var validPalindrome = function(s) {
     for (let i = 0; i < s.length; i++) {
-        
+        let left = s.substring(0, i);
+        let right = s.substring(i + 1);
+        let str = left + right;
+        if (isPalindrome(str)) {
+            return true;
+        }
     }
+    return false;
 };
 // @lc code=end
