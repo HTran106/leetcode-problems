1e736cf10096a94b71659abccf18b6dee5beac52
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-02 13:19:17 -0800

    working on diagonalSum

--- 1572.matrix-diagonal-sum.js
+++ 1572.matrix-diagonal-sum.js
@@ -16,7 +16,22 @@ var diagonalSum = function(mat) {
     let col2 = mat[0].length - 1
     let res = 0
     while (row1 !== mat.length && row2 !== -1) {
-
+        let num1 = mat[row1][col1]
+        let num2 = mat[row2][col2]
+        if (row1 !== row2 && col1 !== col2) {
+            res += num1 + num2
+            row1++
+            col1++
+            row2--
+            col2--
+        } else {
+            res += num1
+            row1++
+            col1++
+            row2--
+            col2--
+        }
     }
+    return res
 };
 // @lc code=end
