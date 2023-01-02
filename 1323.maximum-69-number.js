95a802ab9307c97235bd4a2d7a525f1ad2d3107f
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-02 13:05:34 -0800

    working on max69Num

---
+++ 1323.maximum-69-number.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=1323 lang=javascript
+ *
+ * [1323] Maximum 69 Number
+ */
+
+// @lc code=start
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximum69Number  = function(num) {
+    num = num.toString()
+    console.log(num)
+    num.replace(6, 9)
+    return +num
+};
+// @lc code=end
