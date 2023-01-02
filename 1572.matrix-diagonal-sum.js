4a95534c1c4249b3a40dd664017f377872f010d0
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-02 13:17:04 -0800

    working on diagonalSum

---
+++ 1572.matrix-diagonal-sum.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=1572 lang=javascript
+ *
+ * [1572] Matrix Diagonal Sum
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} mat
+ * @return {number}
+ */
+var diagonalSum = function(mat) {
+    let row1 = 0
+    let col1 = 0
+    let row2 = mat[0].length - 1
+    let col2 = mat[0].length - 1
+    let res = 0
+    while (row1 !== mat.length && row2 !== -1) {
+
+    }
+};
+// @lc code=end
