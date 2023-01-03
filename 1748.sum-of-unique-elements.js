14ad536635abdf1bf04cd7da3d57ba812d842192
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-03 13:27:27 -0800

    working on sumOfUnique

--- 1748.sum-of-unique-elements.js
+++ 1748.sum-of-unique-elements.js
@@ -10,13 +10,6 @@
  * @return {number}
  */
 var sumOfUnique = function (nums) {
-    let resArr = []
-    nums.forEach((num, i) => {
-        if (!resArr.includes(num)) resArr.push(num)
-        else {
-            resArr.splice(resArr.indexOf(num), 1)
-        }
-    })
-    return resArr.reduce((a, c) => a + c)
+    
 };
 // @lc code=end
