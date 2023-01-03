fcbd3c195a65aa5ebcb09f3befa7166b8ef54cbb
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-03 13:30:54 -0800

    done with sumOfUnique

--- 1748.sum-of-unique-elements.js
+++ 1748.sum-of-unique-elements.js
@@ -10,6 +10,15 @@
  * @return {number}
  */
 var sumOfUnique = function (nums) {
-    
+    let count = {}
+    let sum = 0
+    nums.forEach(num => {
+        count[num] ? count[num]++ : count[num] = 1
+    })
+    console.log(count)
+    for (let k in count) {
+        if (count[k] === 1) sum += +k
+    }
+    return sum
 };
 // @lc code=end
