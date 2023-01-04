c8c8a71164558a674729e296825180aef8051226
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-04 09:17:53 -0800

    workign on countMatches

--- 1773.count-items-matching-a-rule.js
+++ 1773.count-items-matching-a-rule.js
@@ -13,12 +13,12 @@
  */
 var countMatches = function(items, ruleKey, ruleValue) {
     // let count = 0
-    items.filter(item => {
+    return items.filter(item => {
         const [type, color, name] = item
         if (ruleKey === 'color' && color === ruleValue) return item
         if (ruleKey === 'type' && type === ruleValue) return item
         if (ruleKey === 'name' && name === ruleValue) return item
-    })
+    }).length
     // return count
 };
 // @lc code=end
