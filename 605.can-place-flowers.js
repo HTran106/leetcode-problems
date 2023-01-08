42cf9135391d8f8f6234a385c99fdd3f5e823045
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-08 12:47:40 -0800

    done with flowerBed

--- 605.can-place-flowers.js
+++ 605.can-place-flowers.js
@@ -11,6 +11,15 @@
  * @return {boolean}
  */
 var canPlaceFlowers = function (flowerbed, n) {
-    return flowerbed.filter((e, i) => i % 2 === 0 && e === 0 && flowerbed[i + 1] !== 1 && flowerbed[i - 1] !== 1).length >= n
+    // return flowerbed.filter((e, i) => i % 2 === 0 && e === 0 && flowerbed[i + 1] !== 1 && flowerbed[i - 1] !== 1).length >= n
+    let count = 0
+    for (let i = 0; i < flowerbed.length; i++) {
+        let el = flowerbed[i]
+        if (flowerbed[i - 1] !== 1 && flowerbed[i + 1] !== 1 && el === 0) {
+            flowerbed[i] = 1
+            count++
+        }
+    }
+    return count >= n
 };
 // @lc code=end
