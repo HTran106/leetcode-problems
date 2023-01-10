f58c619a932eb41b5a1811a533cd2a7c729ddb90
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-10 09:28:45 -0800

    working on maximumProduct

---
+++ 628.maximum-product-of-three-numbers.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=628 lang=javascript
+ *
+ * [628] Maximum Product of Three Numbers
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumProduct = function(nums) {
+    
+};
+// @lc code=end
+
