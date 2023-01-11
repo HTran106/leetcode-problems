0837166d1cb6c54963059302e043dbbe4d596d94
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-11 10:57:06 -0800

    working on flipandinvert

---
+++ 832.flipping-an-image.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=832 lang=javascript
+ *
+ * [832] Flipping an Image
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} image
+ * @return {number[][]}
+ */
+var flipAndInvertImage = function(image) {
+    const reversed = image.map(arr => arr.split('').reverse())
+    
+};
+// @lc code=end
