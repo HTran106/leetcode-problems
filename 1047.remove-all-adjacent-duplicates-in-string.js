3f537b9c926be0c4e64b8777ed6718c25202cb0e
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-12 11:06:05 -0800

    working on remove duplicates

---
+++ 1047.remove-all-adjacent-duplicates-in-string.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1047 lang=javascript
+ *
+ * [1047] Remove All Adjacent Duplicates In String
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicates = function(s) {
+    
+};
+// @lc code=end
+
