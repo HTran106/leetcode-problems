e90e8de53e07255c8d68c901f6e3f400a4d18b14
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-13 11:40:16 -0800

    nothing

---
+++ 1684.count-the-number-of-consistent-strings.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1684 lang=javascript
+ *
+ * [1684] Count the Number of Consistent Strings
+ */
+
+// @lc code=start
+/**
+ * @param {string} allowed
+ * @param {string[]} words
+ * @return {number}
+ */
+var countConsistentStrings = function(allowed, words) {
+    
+};
+// @lc code=end
+
