a7cda272cc7093162d5fbea19172e069269aeb50
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-15 10:51:28 -0800

    working on commonChars

---
+++ 1002.find-common-characters.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1002 lang=javascript
+ *
+ * [1002] Find Common Characters
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var commonChars = function(words) {
+    let res = []
+};
+// @lc code=end
