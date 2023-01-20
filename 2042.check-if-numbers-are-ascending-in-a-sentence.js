5958137c4b6ef4e01da031d153ada2e8d4a8c341
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-19 21:05:04 -0800

    working on areNumbersAscending

--- 2042.check-if-numbers-are-ascending-in-a-sentence.js
+++ 2042.check-if-numbers-are-ascending-in-a-sentence.js
@@ -13,6 +13,8 @@ var areNumbersAscending = function(s) {
     const nums = s.match(/\d+/g)
     if (nums === null) return true
     for (let i = 0; i < nums.length - 1; i++) {
-        
+        if (nums[i] >= nums[i + 1]) return false
+    }
+    return true
 };
 // @lc code=end
