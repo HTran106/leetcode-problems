826bf5c658c527287027338d96f69b12e344bfb1
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-19 21:04:38 -0800

    working on areNumbersAscending

---
+++ 2042.check-if-numbers-are-ascending-in-a-sentence.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=2042 lang=javascript
+ *
+ * [2042] Check if Numbers Are Ascending in a Sentence
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var areNumbersAscending = function(s) {
+    const nums = s.match(/\d+/g)
+    if (nums === null) return true
+    for (let i = 0; i < nums.length - 1; i++) {
+        
+};
+// @lc code=end
