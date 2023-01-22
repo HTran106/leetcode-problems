e1edccaa305711a47c42d5429a292446a7bfc05a
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-22 12:46:25 -0800

    working on findRestaurant

---
+++ 599.minimum-index-sum-of-two-lists.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=599 lang=javascript
+ *
+ * [599] Minimum Index Sum of Two Lists
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} list1
+ * @param {string[]} list2
+ * @return {string[]}
+ */
+var findRestaurant = function(list1, list2) {
+    let currMax = 0
+    for (let i = 0; i < list1.length; i++) {
+        
+    }
+};
+// @lc code=end
