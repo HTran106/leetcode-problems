2fc7167cc94bda5b14558d010cf46ad950b30739
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-23 13:09:12 -0800

    done with calPoints

--- 682.baseball-game.js
+++ 682.baseball-game.js
@@ -16,12 +16,17 @@ var calPoints = function(operations) {
             let prev1 = res[res.length - 1]
             let prev2 = res[res.length - 2]
             res.push(prev1 + prev2)
-        }
-
-        if (operation === D) {
+        } else if (operation === 'D') {
             let prev = res[res.length - 1]
             res.push(prev * 2)
+        } else if (operation === 'C') {
+            res.pop()
+        } else {
+            res.push(+operation)
         }
     })
+
+    if (res.length === 0) return 0
+    else return res.reduce((a, c) => a + c)
 };
 // @lc code=end
