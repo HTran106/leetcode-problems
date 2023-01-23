b914dc02e09ab267404d74043a934a050d3aecb0
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-23 13:02:44 -0800

    working on calPoints

---
+++ 682.baseball-game.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=682 lang=javascript
+ *
+ * [682] Baseball Game
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var calPoints = function(operations) {
+    let res = 0
+    operations.forEach(operation => {
+        
+    })
+};
+// @lc code=end
