4adab23210b2c31f94f24a01b170bed80e30325b
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-24 13:12:29 -0800

    working on selfDividingNumbers

---
+++ 728.self-dividing-numbers.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=728 lang=javascript
+ *
+ * [728] Self Dividing Numbers
+ */
+
+// @lc code=start
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+var selfDividingNumbers = function(left, right) {
+    let res = []
+    for (let i = left; i <= right; i++) {
+        let nums = i.toString().split('')
+
+    }
+};
+// @lc code=end
