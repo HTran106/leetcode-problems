30c36843333de199a11aedf110f45f80adabbfc1
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-25 13:44:12 -0800

    working on minCostClimbingStairs

--- 746.min-cost-climbing-stairs.js
+++ 746.min-cost-climbing-stairs.js
@@ -10,15 +10,13 @@
  * @return {number}
  */
 var minCostClimbingStairs = function (cost) {
-    let total = 0
-    for (let i = 0; i < cost.length; i++) {
-        // console.log(cost[i])
-        if (cost[i] < cost[i + 1]) {
-            total += +cost[i]
-        } else {
-            total += +cost[i + 1]
-        }
+    let n = cost.length;
+    let dp = new Array(n + 1);
+    dp[0] = 0;
+    dp[1] = 0;
+    for (let i = 2; i <= n; i++) {
+        dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
     }
-    return total
+    return dp[n];
 };
 // @lc code=end
