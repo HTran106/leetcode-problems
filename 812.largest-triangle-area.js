02103e261b1c362aab6c0fc93d64b0a5cc5b3509
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-26 13:48:04 -0800

    working on largestTriangleArea

---
+++ 812.largest-triangle-area.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=812 lang=javascript
+ *
+ * [812] Largest Triangle Area
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var largestTriangleArea = function(points) {
+    let maxArea = 0;
+    for (let i = 0; i < points.length; i++) {
+        
+};
+// @lc code=end
