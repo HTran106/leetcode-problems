0d1a55560ba35b66fdd963df45c255c1622576d8
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-26 13:57:37 -0800

    working on mostCommonWord

--- 819.most-common-word.js
+++ 819.most-common-word.js
@@ -10,14 +10,31 @@
  * @param {string[]} banned
  * @return {string}
  */
-var mostCommonWord = function(paragraph, banned) {
+var mostCommonWord = function (paragraph, banned) {
     let words = paragraph.split(' ');
+    let currLength = 0;
     words.map(word => {
         word = word.replace(/[^a-zA-Z]/g, '').toLowerCase();
         return word;
     })
-    words.filter(word => {
-        
+    let wordCount = {};
+    words.forEach(word => {
+        if (wordCount[word] === undefined) {
+            wordCount[word] = 1;
+        } else {
+            wordCount[word]++;
+        }
     })
+    
+    let maxCount = 0;
+    let maxWord = '';
+
+    for (let word in wordCount) {
+        if (wordCount[word] > maxCount && !banned.includes(word)) {
+            maxCount = wordCount[word];
+            maxWord = word;
+        }
+    }
+    return maxWord;
 };
 // @lc code=end
