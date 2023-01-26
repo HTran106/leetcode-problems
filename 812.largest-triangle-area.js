87d88943c4c7a0deacb61212c2a2660a37e2c0a8
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-26 13:48:34 -0800

    done with largestTriangleArea

--- 812.largest-triangle-area.js
+++ 812.largest-triangle-area.js
@@ -12,6 +12,13 @@
 var largestTriangleArea = function(points) {
     let maxArea = 0;
     for (let i = 0; i < points.length; i++) {
-        
+        for (let j = i + 1; j < points.length; j++) {
+            for (let k = j + 1; k < points.length; k++) {
+                let area = Math.abs(points[i][0] * points[j][1] + points[j][0] * points[k][1] + points[k][0] * points[i][1] - points[i][1] * points[j][0] - points[j][1] * points[k][0] - points[k][1] * points[i][0]) / 2;
+                maxArea = Math.max(maxArea, area);
+            }
+        }
+    }
+    return maxArea;
 };
 // @lc code=end
