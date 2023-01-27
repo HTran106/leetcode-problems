d019c6808799f37e65566168cdeb1639c53b9974
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-27 14:19:06 -0800

    working on goatLatin

---
+++ 824.goat-latin.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=824 lang=javascript
+ *
+ * [824] Goat Latin
+ */
+
+// @lc code=start
+/**
+ * @param {string} sentence
+ * @return {string}
+ */
+var toGoatLatin = function(sentence) {
+    
+};
+// @lc code=end
+
