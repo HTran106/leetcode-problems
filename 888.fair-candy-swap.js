f5788209d05c2cd47e2858d18afff0d733a5e641
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-28 14:31:55 -0800

    working on fairCandySwap

---
+++ 888.fair-candy-swap.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=888 lang=javascript
+ *
+ * [888] Fair Candy Swap
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} aliceSizes
+ * @param {number[]} bobSizes
+ * @return {number[]}
+ */
+var fairCandySwap = function(aliceSizes, bobSizes) {
+    const sumA = aliceSizes.reduce((a, b) => a + b, 0);
+    const sumB = bobSizes.reduce((a, b) => a + b, 0);
+    
+};
+// @lc code=end
