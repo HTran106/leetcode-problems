b57482f85ebeda1f2f18820153c762c7cda7e881
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-28 14:32:30 -0800

    done with fairCandySwap

--- 888.fair-candy-swap.js
+++ 888.fair-candy-swap.js
@@ -13,6 +13,18 @@
 var fairCandySwap = function(aliceSizes, bobSizes) {
     const sumA = aliceSizes.reduce((a, b) => a + b, 0);
     const sumB = bobSizes.reduce((a, b) => a + b, 0);
-    
+
+    const diff = (sumA - sumB) / 2;
+
+    const setB = new Set(bobSizes);
+
+    for (let x of aliceSizes) {
+        if (setB.has(x - diff)) {
+            return [x, x - diff];
+        }
+    }
+
+    return [];
+
 };
 // @lc code=end
