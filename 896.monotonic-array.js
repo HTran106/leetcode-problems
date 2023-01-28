2f19b65106b631f836027fbdad7eaeae398e16b6
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-28 14:30:56 -0800

    done with isMonotonic

---
+++ 896.monotonic-array.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=896 lang=javascript
+ *
+ * [896] Monotonic Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isMonotonic = function(nums) {
+    let increasing = true;
+    let decreasing = true;
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] > nums[i + 1]) {
+            increasing = false;
+        }
+        if (nums[i] < nums[i + 1]) {
+            decreasing = false;
+        }
+    }
+    return increasing || decreasing;
+};
+// @lc code=end
