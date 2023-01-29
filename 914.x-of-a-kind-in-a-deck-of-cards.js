c1e756231acab50d0dfc8d100729be62b4b8fa27
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-29 14:37:41 -0800

    done with hasGroupsSizeX

--- 914.x-of-a-kind-in-a-deck-of-cards.js
+++ 914.x-of-a-kind-in-a-deck-of-cards.js
@@ -14,5 +14,11 @@ var hasGroupsSizeX = function(deck) {
     for (let i = 0; i < deck.length; i++) {
         count[deck[i]] = count[deck[i]] + 1 || 1
     }
+    let min = Math.min(...Object.values(count))
+    if (min < 2) return false
+    for (let i = 2; i <= min; i++) {
+        if (Object.values(count).every(v => v % i === 0)) return true
+    }
+    return false
 };
 // @lc code=end
