29f73a8e3d2c4ad74ae7312f76ccf846afcadac0
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-29 14:48:22 -0800

    working on isLongPressedName

--- 925.long-pressed-name.js
+++ 925.long-pressed-name.js
@@ -10,12 +10,20 @@
  * @param {string} typed
  * @return {boolean}
  */
-var isLongPressedName = function(name, typed) {
+var isLongPressedName = function (name, typed) {
     const nameArr = name.split('');
     const typedArr = typed.split('');
     let i = 0;
     let j = 0;
     while (i < nameArr.length && j < typedArr.length) {
-        
+        if (nameArr[i] === typedArr[j]) {
+            i++;
+            j++;
+        } else if (i > 0 && nameArr[i - 1] === typedArr[j]) {
+            j++;
+        } else {
+            return false;
+        }
+    }
 };
 // @lc code=end
