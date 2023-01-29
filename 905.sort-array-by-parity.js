3b686a1dda36b3cfdb2f5276f0452e5d07017f7c
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-29 14:33:42 -0800

    working on sortArrayByParity

---
+++ 905.sort-array-by-parity.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=905 lang=javascript
+ *
+ * [905] Sort Array By Parity
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function(nums) {
+    const evens = nums.filter(num => num % 2 === 0);
+    
+};
+// @lc code=end
