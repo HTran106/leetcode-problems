a28e19e8bc6786c88350acefd82fdcbb2ac744ac
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-30 14:58:28 -0800

    working on divisorGame

---
+++ 1025.divisor-game.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1025 lang=javascript
+ *
+ * [1025] Divisor Game
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var divisorGame = function(n) {
+    const dp = new Array(n + 1).fill(false);
+    
+};
+// @lc code=end
