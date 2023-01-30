5c771b72f21df963be13ec81c235b77e411b147d
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-30 14:53:19 -0800

    finished with sortedSquare

---
+++ 977.squares-of-a-sorted-array.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=977 lang=javascript
+ *
+ * [977] Squares of a Sorted Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    return nums.map(num => num * num).sort((a, b) => a - b);
+};
+// @lc code=end
