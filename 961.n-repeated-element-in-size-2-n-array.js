b660ab780d2246382054ad07a1a0d7a9ce92808e
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-30 14:51:43 -0800

    working on repeatedNTimes

---
+++ 961.n-repeated-element-in-size-2-n-array.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=961 lang=javascript
+ *
+ * [961] N-Repeated Element in Size 2N Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var repeatedNTimes = function(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        
+    }
+};
+// @lc code=end
