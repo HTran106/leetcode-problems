0c818803824eea1654e64c0480f84539432e17e3
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-01-31 14:59:18 -0800

    working on divisorGame

--- 1025.divisor-game.js
+++ 1025.divisor-game.js
@@ -14,6 +14,12 @@ var divisorGame = function(n) {
     dp[1] = false;
     dp[2] = true;
     for (let i = 3; i <= n; i++) {
-        
+        for (let j = 1; j < i; j++) {
+            if (i % j === 0 && !dp[i - j]) {
+                dp[i] = true;
+                break;
+            }
+        }
+    }
 };
 // @lc code=end
