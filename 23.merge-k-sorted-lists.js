07b437fb67101b2aa24f0982a3f97627c101504b
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-02-01 15:03:16 -0800

    working on mergeKLists

--- 23.merge-k-sorted-lists.js
+++ 23.merge-k-sorted-lists.js
@@ -61,7 +61,34 @@ class MinHeap {
     }
 
     bubbleDown() {
-        
+        let index = 0;
+        const length = this.heap.length;
+        const element = this.heap[0];
+        while (true) {
+            const leftChildIndex = 2 * index + 1;
+            const rightChildIndex = 2 * index + 2;
+            let swap = null;
+            let leftChild;
+
+            if (leftChildIndex < length) {
+                leftChild = this.heap[leftChildIndex];
+                if (leftChild.val < element.val) {
+                    swap = leftChildIndex;
+                }
+            }
+
+            if (rightChildIndex < length) {
+                const rightChild = this.heap[rightChildIndex];
+                if (
+                    (swap === null && rightChild.val < element.val) ||
+                    (swap !== null && rightChild.val < leftChild.val)
+                ) {
+                    swap = rightChildIndex;
+                }
+            }
+
+            
+
 
 
 
