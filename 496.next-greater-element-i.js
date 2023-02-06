a5e4ecfa679896b807c27c9edc9dbb3baed988ad
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-02-06 12:23:23 -0800

    working on nextGreaterElement

--- 496.next-greater-element-i.js
+++ 496.next-greater-element-i.js
@@ -11,7 +11,18 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1, nums2) {
-    
+    const res = [];
+    for (let i = 0; i < nums1.length; i++) {
+        let num = nums1[i]
+        for (let j = nums2.indexOf(num) + 1; j < nums2.length; j++) {
+            let num2 = nums2[j]
+            if (num2 > num) {
+                res.push(num2)
+                break
+            }
+            if (num2 < num && j === num2.length - 1) res.push(-1)
+        }
+    }
+    return res
 };
 // @lc code=end
-
