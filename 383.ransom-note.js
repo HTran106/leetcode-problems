1a70a15b3b40401764da3bf121bb12fd2bd4823f
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-02-10 11:30:08 -0800

    working on canConstruct

--- 383.ransom-note.js
+++ 383.ransom-note.js
@@ -11,9 +11,22 @@
  * @return {boolean}
  */
 var canConstruct = function(ransomNote, magazine) {
-    for (let i = 0; i < ransomNote.length; i++) {
-        
+    // return magazine.length >= ransomNote.length && [...ransomNote].every(c => {
+    //     let i = magazine.indexOf(c);
+    //     if (i >= 0) {
+    //         magazine = magazine.slice(0, i) + magazine.slice(i + 1);
+    //         return true;
+    //     }
+    //     return false;
+    // }
+    // );
+    // magazine = magazine.split('')
+    // console.log('abcdef'.slice(2))
+    for (let char of ransomNote) {
+        if (magazine.indexOf(char) !== -1) magazine = magazine.slice(0, magazine.indexOf(char)) + magazine.slice(magazine.indexOf(char) + 1)
+        else return false
     }
+    return true
 };
 // @lc code=end
 
