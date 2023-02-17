06df5905024c22c7d9c76e6ed6fcaa03f7d76ace
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-02-17 13:36:15 -0800

    done with pivot index

--- 724.find-pivot-index.js
+++ 724.find-pivot-index.js
@@ -10,10 +10,13 @@
  * @return {number}
  */
 var pivotIndex = function(nums) {
+    let sum = nums.reduce((acc, num) => acc + num, 0);
+    let leftSum = 0;
     for (let i = 0; i < nums.length; i++) {
-        let left = nums.slice(0, i).reduce((a, b) => a + b, 0);
-        let right = nums.slice(i + 1).reduce((a, b) => a + b, 0);
-        if (left === right) return i;
+        if (leftSum === sum - leftSum - nums[i]) {
+            return i;
+        }
+        leftSum += nums[i];
     }
     return -1;
 };
