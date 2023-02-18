9f5cd7bc018cea66c771621fc702e106ff5db04c
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-02-18 13:39:02 -0800

    working on large group

---
+++ 830.positions-of-large-groups.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=830 lang=javascript
+ *
+ * [830] Positions of Large Groups
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number[][]}
+ */
+var largeGroupPositions = function(s) {
+    for (let char of s) {
+        
+    }
+};
+// @lc code=end
