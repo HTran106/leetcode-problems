e6a26a7593de3397217a657891d25bced3862f5e
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-02-23 11:40:05 -0800

    findContentChildren

---
+++ 455.assign-cookies.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=455 lang=javascript
+ *
+ * [455] Assign Cookies
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function(g, s) {
+    let count = 0;
+    g.forEach(el => {
+        
+    })
+};
+// @lc code=end
