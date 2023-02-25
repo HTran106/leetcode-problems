688abd53d9dbaa41736d890c3a8f1f02033b0a6e
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-02-25 12:01:38 -0800

    done with transpose

---
+++ 867.transpose-matrix.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=867 lang=javascript
+ *
+ * [867] Transpose Matrix
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} matrix
+ * @return {number[][]}
+ */
+var transpose = function(matrix) {
+    const res = [];
+    for (let i = 0; i < matrix[0].length; i++) {
+        res.push([]);
+    }
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            res[j].push(matrix[i][j]);
+        }
+    }
+    return res;
+};
+// @lc code=end
