1f8e2e1290d264eeb7d9c8aa46bc8a5d9199e792
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-02-25 12:03:34 -0800

    transpose done

--- 867.transpose-matrix.js
+++ 867.transpose-matrix.js
@@ -11,14 +11,13 @@
  */
 var transpose = function(matrix) {
     const res = [];
-    // for (let i = 0; i < matrix[0].length; i++) {
-    //     res.push([]);
-    // }
-    // for (let i = 0; i < matrix.length; i++) {
-    //     for (let j = 0; j < matrix[0].length; j++) {
-    //         res[j].push(matrix[i][j]);
-    //     }
-    // }
-    // return res;
+    for (let i = 0; i < matrix[0].length; i++) {
+        const temp = []
+        for (let j = 0; j < matrix.length; j++) {
+            temp.push(matrix[j][i])
+        }
+        res.push(temp)
+    }
+    return res
 };
 // @lc code=end
