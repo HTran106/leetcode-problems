047f657d7fb505a6c3441657da271e70e2315349
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-02-26 12:06:49 -0800

    sortArray

---
+++ 922.sort-array-by-parity-ii.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=922 lang=javascript
+ *
+ * [922] Sort Array By Parity II
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParityII = function(nums) {
+    let even = nums.filter(num => num % 2 === 0)
+    
+};
+// @lc code=end
