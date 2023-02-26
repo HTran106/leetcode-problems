f5002605e534416e13f6a9aa08f42dd66da469b3
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-02-26 12:07:23 -0800

    sortArrayByParity

--- 922.sort-array-by-parity-ii.js
+++ 922.sort-array-by-parity-ii.js
@@ -13,6 +13,13 @@ var sortArrayByParityII = function(nums) {
     let even = nums.filter(num => num % 2 === 0)
     let odd = nums.filter(num => num % 2 !== 0)
     let res = []
+    for (let i = 0; i < nums.length; i++) {
+        if (i % 2 === 0) {
+            res.push(even.shift())
+        } else {
+            res.push(odd.shift())
+        }
+    }
 
 };
 // @lc code=end
