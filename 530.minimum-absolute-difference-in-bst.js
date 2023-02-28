51b21e2a20c2a432129941b417cc267157b0b5eb
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-02-28 09:35:08 -0800

    working on getMinimumDifference

---
+++ 530.minimum-absolute-difference-in-bst.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=530 lang=javascript
+ *
+ * [530] Minimum Absolute Difference in BST
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var getMinimumDifference = function(root) {
+    const queue = [root];
+    const values = [];
+    while (queue.length)
+};
+// @lc code=end
