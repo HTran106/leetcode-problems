47bca01163c7d937dfe54b55eaee36de934d493d
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-03-02 09:03:31 -0800

    done with strStr

---
+++ 3.longest-substring-without-repeating-characters.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=3 lang=javascript
+ *
+ * [3] Longest Substring Without Repeating Characters
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let longest = ''
+    let current = ''
+    for (let i = 0; i < s.length; i++) {
+        
+};
+// @lc code=end
