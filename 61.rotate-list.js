daf4399e3a2a7a132b800238d5a6ac301606d2bd
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-03-03 11:29:34 -0800

    done with rotateRight

--- 61.rotate-list.js
+++ 61.rotate-list.js
@@ -18,7 +18,27 @@
  * @return {ListNode}
  */
 var rotateRight = function(head, k) {
-    let curr = head
-    
+    if (!head || !head.next) {
+        return head;
+    }
+    let length = 1;
+    let tail = head;
+    while (tail.next) {
+        length++;
+        tail = tail.next;
+    }
+    k %= length;
+    if (k === 0) {
+        return head;
+    }
+    let newTail = head;
+    for (let i = 0; i < length - k - 1; i++) {
+        newTail = newTail.next;
+    }
+    let newHead = newTail.next;
+    newTail.next = null;
+    tail.next = head;
+    return newHead;
+
 };
 // @lc code=end
