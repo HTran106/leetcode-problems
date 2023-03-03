440a3102ee67be08bef2177b9fa1137f27ba8423
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-03-03 11:24:29 -0800

    working on rotateRight

--- 54.spiral-matrix.js
+++ 54.spiral-matrix.js
@@ -11,43 +11,32 @@
  */
 var spiralOrder = function(matrix) {
     let result = [];
-
-    if (matrix.length === 1) {
-        result.push(matrix[0][0])
-        return result;
+    let rowStart = 0;
+    let rowEnd = matrix.length - 1;
+    let colStart = 0;
+    let colEnd = matrix[0].length - 1;
+    while (rowStart <= rowEnd && colStart <= colEnd) {
+        for (let i = colStart; i <= colEnd; i++) {
+            result.push(matrix[rowStart][i]);
+        }
+        rowStart++;
+        for (let i = rowStart; i <= rowEnd; i++) {
+            result.push(matrix[i][colEnd]);
+        }
+        colEnd--;
+        if (rowStart <= rowEnd) {
+            for (let i = colEnd; i >= colStart; i--) {
+                result.push(matrix[rowEnd][i]);
+            }
+        }
+        rowEnd--;
+        if (colStart <= colEnd) {
+            for (let i = rowEnd; i >= rowStart; i--) {
+                result.push(matrix[i][colStart]);
+            }
+        }
+        colStart++;
     }
-
-    let max = matrix[0].length - 1;
-
-    // Grab the first row | result.push.apply(result,matrix[0])
-    for (let i = 0; i <= max; i++) {
-        result.push(matrix[0][i]);
-    }
-
-    // Grab the last column
-    for (let i = 1; i < max; i++) {
-        result.push(matrix[i][max]);
-    }
-
-    // Grab the last row
-    for (let i = max; i >= 0; i--) {
-        result.push(matrix[max][i]);
-    }
-
-    // Grab the first column
-    for (let i = max - 1; i > 0; i--) {
-        result.push(matrix[i][0]);
-    }
-
-    let submatrix = [];
-    // Form the inner matrix
-    for (let i = 1; i < max; i++) {
-        submatrix.push(matrix[i].splice(1, max - 1));
-    }
-
-    //call it recursively
-    result = result.concat(spiralOrder(submatrix));
-
     return result;
 };
 // @lc code=end
