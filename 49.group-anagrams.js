f667f1e5891b10c83a179632c97e41bec7b6222f
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-03-03 09:11:27 -0800

    working on groupAnagrams

--- 49.group-anagrams.js
+++ 49.group-anagrams.js
@@ -12,7 +12,11 @@
 var groupAnagrams = function(strs) {
     let res = []
     for (let i = 0; i < strs.length; i++) {
+        let word1 = strs[i].split('')
+        .sort((a, b) => a - b)
+        .join('')
         for (let j = i + 1; j < strs.length; j++) {
+            let word2 = strs[j].split('')
             
         }
     }
