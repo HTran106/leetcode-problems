c79985fd833220aab296b6dd956b533058ffa0a0
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-03-03 09:28:38 -0800

    working on myPow

---
+++ 50.pow-x-n.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=50 lang=javascript
+ *
+ * [50] Pow(x, n)
+ */
+
+// @lc code=start
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    if (n === 0) {
+        return 1
+    }
+};
+// @lc code=end
