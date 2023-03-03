cb2af8330a9feb1da6cd89aaaf51cf4f8aaac215
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-03-03 11:33:50 -0800

    done with search

---
+++ 81.search-in-rotated-sorted-array-ii.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=81 lang=javascript
+ *
+ * [81] Search in Rotated Sorted Array II
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+var search = function(nums, target) {
+    return nums.indexOf(target) !== -1;
+};
+// @lc code=end
