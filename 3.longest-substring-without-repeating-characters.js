4eeb5f815613b3d676864eb9dd5912a34e501eeb
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-03-03 09:05:26 -0800

    done with lengthOfLongestSubstring

--- 3.longest-substring-without-repeating-characters.js
+++ 3.longest-substring-without-repeating-characters.js
@@ -13,6 +13,19 @@ var lengthOfLongestSubstring = function(s) {
     let longest = ''
     let current = ''
     for (let i = 0; i < s.length; i++) {
-        
+        let char = s[i]
+        if (current.includes(char)) {
+            if (current.length > longest.length) {
+                longest = current
+            }
+            current = current.slice(current.indexOf(char) + 1) + char
+        } else {
+            current += char
+        }
+    }
+    if (current.length > longest.length) {
+        longest = current
+    }
+    return longest.length
 };
 // @lc code=end
