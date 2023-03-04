2e3ad188c59917c0011a5c8d09776f5ae7a5af0c
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-03-04 10:27:50 -0800

    done with mergeIntervals

---
+++ 56.merge-intervals.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=56 lang=javascript
+ *
+ * [56] Merge Intervals
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    if (intervals.length === 0) {
+        return [];
+    }
+    intervals.sort((a, b) => a[0] - b[0]);
+    let res = [intervals[0]];
+    for (let i = 1; i < intervals.length; i++) {
+        if (res[res.length - 1][1] >= intervals[i][0]) {
+        res[res.length - 1][1] = Math.max(res[res.length - 1][1], intervals[i][1]);
+        } else {
+        res.push(intervals[i]);
+        }
+    }
+    return res;
+};
+// @lc code=end
