3548165dcdbf6df340cda4606d3fc0e7035697fa
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-03-05 10:29:51 -0800

    done with uniquePaths2

---
+++ 63.unique-paths-ii.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=63 lang=javascript
+ *
+ * [63] Unique Paths II
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} obstacleGrid
+ * @return {number}
+ */
+var uniquePathsWithObstacles = function(obstacleGrid) {
+    let m = obstacleGrid.length;
+    let n = obstacleGrid[0].length;
+    let dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
+    for (let i = 0; i < m; i++) {
+        if (obstacleGrid[i][0] === 1) {
+            break;
+        }
+        dp[i][0] = 1;
+    }
+    for (let j = 0; j < n; j++) {
+        if (obstacleGrid[0][j] === 1) {
+            break;
+        }
+        dp[0][j] = 1;
+    }
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            if (obstacleGrid[i][j] === 1) {
+                dp[i][j] = 0;
+            } else {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+    }
+    return dp[m - 1][n - 1];
+};
+// @lc code=end
