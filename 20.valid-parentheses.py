31a5be13a9e6c73a08d1bdd30ccbe7ae3cd08de5
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-03-10 13:51:56 -0800

    working on valid Parentheses

---
+++ 20.valid-parentheses.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=20 lang=python
+#
+# [20] Valid Parentheses
+#
+
+# @lc code=start
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        for char in s:
+            
+
+# @lc code=end
