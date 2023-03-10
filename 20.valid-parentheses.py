48b7b70680892e2a5e1643e65548de6858ffe2f9
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-03-10 13:52:57 -0800

    working on valid Parentheses

--- 20.valid-parentheses.py
+++ 20.valid-parentheses.py
@@ -15,6 +15,18 @@ class Solution(object):
         for char in s:
             if char in ['(', '[', '{']:
                 stack.append(char)
+            else:
+                if len(stack) == 0:
+                    return False
+                else:
+                    if char == ')' and stack[-1] == '(':
+                        stack.pop()
+                    elif char == ']' and stack[-1] == '[':
+                        stack.pop()
+                    elif char == '}' and stack[-1] == '{':
+                        stack.pop()
+                    else:
+                        return False
 
 
 # @lc code=end
