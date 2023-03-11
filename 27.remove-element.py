c6828d0256dc5f772c10d177613067d1f0b7394e
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-03-11 13:54:56 -0800

    working on removeElement

--- 27.remove-element.py
+++ 27.remove-element.py
@@ -13,4 +13,10 @@ class Solution(object):
         :rtype: int
         """
         i = 0
+        while i < len(nums):
+            if nums[i] == val:
+                nums.pop(i)
+            else:
+                i += 1
+        return len(nums)
 # @lc code=end
