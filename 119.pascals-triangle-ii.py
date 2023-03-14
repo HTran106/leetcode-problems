f36e128f6d659b611338aa11ed31dc63a8544746
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-03-14 14:09:02 -0700

    working on pascal's triangle 2

---
+++ 119.pascals-triangle-ii.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=119 lang=python
+#
+# [119] Pascal's Triangle II
+#
+
+# @lc code=start
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        if rowIndex == 0:
+            return [1]
+
+# @lc code=end
