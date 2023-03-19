2b19d04f0c07725bdeaec9b013d42a3828a1b56b
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-03-19 10:50:21 -0700

    working on twoNumbers

--- 2.add-two-numbers.py
+++ 2.add-two-numbers.py
@@ -21,5 +21,17 @@ class Solution(object):
         head = ListNode(0)
         curr = head
         carry = 0
-        
+
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+
+            curr.next = ListNode(carry % 10)
+            curr = curr.next
+            carry //= 10
+
 # @lc code=end
