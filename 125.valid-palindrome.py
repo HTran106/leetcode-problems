cb309ca088a1600476bdbb2b245b203adc0f3b9d
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-03-21 10:56:28 -0700

    working on isPalindrome

---
+++ 125.valid-palindrome.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=125 lang=python
+#
+# [125] Valid Palindrome
+#
+
+# @lc code=start
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        string = ""
+        for char in s:
+            if char.isalnum():
+                string += char.lower()
+                
+
+# @lc code=end
