0978e8459eff4d015e99cd62605542673b7f3a4a
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-03-21 10:57:00 -0700

    done with isPalindrome

--- 125.valid-palindrome.py
+++ 125.valid-palindrome.py
@@ -16,6 +16,8 @@ class Solution(object):
         for char in s:
             if char.isalnum():
                 string += char.lower()
-                
+
+        return string == string[::-1]
+    
 
 # @lc code=end
