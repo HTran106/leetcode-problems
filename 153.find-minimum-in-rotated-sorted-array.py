22892ba17612fb9e422cc1f1d2bb2b2de88b7935
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-03-22 10:59:09 -0700

    working on findMin

---
+++ 153.find-minimum-in-rotated-sorted-array.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=153 lang=python
+#
+# [153] Find Minimum in Rotated Sorted Array
+#
+
+# @lc code=start
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        if len(nums) == 1:
+            return nums[0]
+
+
+
+
+# @lc code=end
