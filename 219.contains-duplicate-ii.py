f9a355b4f938a0e78830b447d91a5cd9a842e0ab
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-03-23 11:01:41 -0700

    working on containsNearbyDuplicate

---
+++ 219.contains-duplicate-ii.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=219 lang=python
+#
+# [219] Contains Duplicate II
+#
+
+# @lc code=start
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+
+        if len(nums) == len(set(nums)):
+            return False
+
+        
+
+# @lc code=end
