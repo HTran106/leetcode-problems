4687d269bba376a8d3b092344347914ae76355bb
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-03-29 07:43:31 -0700

    done with moveZeroes

--- 283.move-zeroes.py
+++ 283.move-zeroes.py
@@ -16,6 +16,13 @@ class Solution(object):
             return
 
         left, right = 0, 0
-        
+        while right < len(nums):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+            right += 1
+
+            
+
 
 # @lc code=end
