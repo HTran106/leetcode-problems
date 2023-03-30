408a7373240585607ea264b015ba11ff6b1b6804
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-03-30 08:08:55 -0700

    working on intersectOfTwoArrays II

---
+++ 350.intersection-of-two-arrays-ii.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=350 lang=python
+#
+# [350] Intersection of Two Arrays II
+#
+
+# @lc code=start
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+
+        nums1.sort()
+        nums2.sort()
+
+        i = j = 0
+        
+# @lc code=end
