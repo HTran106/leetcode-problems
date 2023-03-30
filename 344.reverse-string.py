c009008a435010979d62ecbdca8a8768c1d86e5a
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-03-30 08:07:29 -0700

    done with reverseString

---
+++ 344.reverse-string.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=344 lang=python
+#
+# [344] Reverse String
+#
+
+# @lc code=start
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+
+        if len(s) == 1:
+            return
+
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
+# @lc code=end
