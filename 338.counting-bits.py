4248106b7adc2593ce74d84d92ce352dee654ae9
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-03-31 08:12:16 -0700

    done with isSubsequence

---
+++ 338.counting-bits.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=338 lang=python
+#
+# [338] Counting Bits
+#
+
+# @lc code=start
+class Solution(object):
+    def countBits(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+
+    
+
+
+
+# @lc code=end
