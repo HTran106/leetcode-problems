3b3a9452d8700e76d8ad2ec3eb00a4141cda296e
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-04-03 13:46:56 -0700

    wokring on threeSumClosest

--- 16.3-sum-closest.py
+++ 16.3-sum-closest.py
@@ -15,5 +15,9 @@ class Solution(object):
 
         nums.sort()
         closest = None
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1];
+                continue
+            l, r = i + 1, len(nums) - 1
 
 # @lc code=end
