820ef86650ce36e280c7f259c0bc39fead0ea4a5
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-05-16 11:57:20 -0700

    done with numberOfMatches

---
+++ 1688.count-of-matches-in-tournament.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=1688 lang=python
+#
+# [1688] Count of Matches in Tournament
+#
+
+# @lc code=start
+class Solution(object):
+    def numberOfMatches(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+        return n - 1
+# @lc code=end
