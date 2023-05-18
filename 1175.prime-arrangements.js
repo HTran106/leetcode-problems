609fcd22e3000a7e1f29537d02790a87f7546b05
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-05-18 12:14:08 -0700

    working on numPrimeArrangements

---
+++ 1175.prime-arrangements.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1175 lang=javascript
+ *
+ * [1175] Prime Arrangements
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numPrimeArrangements = function(n) {
+    const MOD = 1e9 + 7;
+    
+};
+// @lc code=end
