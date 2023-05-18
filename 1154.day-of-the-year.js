81996652327d1e083a1d40454585b0eb5f048c11
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-05-18 12:13:29 -0700

    done with tribonacci

--- 1154.day-of-the-year.js
+++ 1154.day-of-the-year.js
@@ -13,5 +13,6 @@ var dayOfYear = function(date) {
     const [year, month, day] = date.split('-').map(Number);
     const isLeapYear = year % 4 === 0 && year % 100 !== 0 || year % 400 === 0;
     const days = [31, isLeapYear ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30];
+    return days.slice(0, month - 1).reduce((acc, cur) => acc + cur, 0) + day;
 };
 // @lc code=end
