9decb3bcae58b755c80e19ebf5fa14663e3f6674
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-05-18 12:12:29 -0700

    working on dayOfYear

---
+++ 1154.day-of-the-year.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1154 lang=javascript
+ *
+ * [1154] Day of the Year
+ */
+
+// @lc code=start
+/**
+ * @param {string} date
+ * @return {number}
+ */
+var dayOfYear = function(date) {
+    const [year, month, day] = date.split('-').map(Number);
+};
+// @lc code=end
