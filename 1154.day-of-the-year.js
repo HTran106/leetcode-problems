4d9bc8146b2550a843e56de14648ca675c9230d8
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-05-18 12:12:50 -0700

    working on dayOfYear

--- 1154.day-of-the-year.js
+++ 1154.day-of-the-year.js
@@ -12,5 +12,6 @@
 var dayOfYear = function(date) {
     const [year, month, day] = date.split('-').map(Number);
     const isLeapYear = year % 4 === 0 && year % 100 !== 0 || year % 400 === 0;
+    const days = [31, isLeapYear ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30];
 };
 // @lc code=end
