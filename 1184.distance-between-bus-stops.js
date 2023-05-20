d999a0d401a859927687dcc52d1386b8368e703e
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-05-20 12:20:24 -0700

    working on distanceBetweenBusStops

---
+++ 1184.distance-between-bus-stops.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=1184 lang=javascript
+ *
+ * [1184] Distance Between Bus Stops
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} distance
+ * @param {number} start
+ * @param {number} destination
+ * @return {number}
+ */
+var distanceBetweenBusStops = function(distance, start, destination) {
+    const n = distance.length;
+    let clockwise = 0;
+    
+};
+// @lc code=end
