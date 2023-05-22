e0ac669cebf09eb20914a9b491f5fc99827c3510
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-05-22 12:23:43 -0700

    done with maxNumberOfBalloons

---
+++ 1189.maximum-number-of-balloons.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=1189 lang=javascript
+ *
+ * [1189] Maximum Number of Balloons
+ */
+
+// @lc code=start
+/**
+ * @param {string} text
+ * @return {number}
+ */
+var maxNumberOfBalloons = function(text) {
+    let b = 0, a = 0, l = 0, o = 0, n = 0;
+    for (let i = 0; i < text.length; i++) {
+        switch (text[i]) {
+            case 'b':
+                b++;
+                break;
+            case 'a':
+                a++;
+                break;
+            case 'l':
+                l++;
+                break;
+            case 'o':
+                o++;
+                break;
+            case 'n':
+                n++;
+                break;
+        }
+    }
+    return Math.min(b, a, Math.floor(l / 2), Math.floor(o / 2), n);
+};
+// @lc code=end
