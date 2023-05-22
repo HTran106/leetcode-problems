6b3165b6ae15b6cb636e63aadafa13d3849ac589
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-05-22 12:24:13 -0700

    working on minimumAbsDifference

---
+++ 1200.minimum-absolute-difference.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1200 lang=javascript
+ *
+ * [1200] Minimum Absolute Difference
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} arr
+ * @return {number[][]}
+ */
+var minimumAbsDifference = function(arr) {
+    const res = [];
+    arr.sort((a, b) => a - b);
+    
+};
+// @lc code=end
