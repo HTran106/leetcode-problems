8b5380bd968f25efb27e2154483df3bda5d7f19f
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-05-23 12:24:45 -0700

    working on minimumAbsDifference

--- 1200.minimum-absolute-difference.js
+++ 1200.minimum-absolute-difference.js
@@ -13,6 +13,15 @@ var minimumAbsDifference = function(arr) {
     const res = [];
     arr.sort((a, b) => a - b);
     let minDiff = Infinity;
-    
+    for (let i = 1; i < arr.length; i++) {
+        const diff = arr[i] - arr[i - 1];
+        if (diff < minDiff) {
+            minDiff = diff;
+            res.length = 0;
+            res.push([arr[i - 1], arr[i]]);
+        } else if (diff === minDiff) {
+            res.push([arr[i - 1], arr[i]]);
+        }
+    }
 };
 // @lc code=end
