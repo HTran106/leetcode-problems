7ea07aede821cf2f505b1a34b155f6ea2f961d2c
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-05-24 12:25:43 -0700

    working on unique occurences

---
+++ 1207.unique-number-of-occurrences.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=1207 lang=javascript
+ *
+ * [1207] Unique Number of Occurrences
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function(arr) {
+    const map = {};
+    for (let i = 0; i < arr.length; i++) {
+        map[arr[i]] = (map[arr[i]] || 0) + 1;
+    }
+    
+};
+// @lc code=end
