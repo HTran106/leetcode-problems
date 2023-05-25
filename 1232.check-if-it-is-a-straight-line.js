801aa4c8e1ef45bfaf5159663635ee4f97d491ca
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-05-25 12:27:56 -0700

    done with checkStraightLine

--- 1232.check-if-it-is-a-straight-line.js
+++ 1232.check-if-it-is-a-straight-line.js
@@ -14,6 +14,12 @@ var checkStraightLine = function(coordinates) {
     const [x1, y1] = coordinates[1];
     const dx = x1 - x0;
     const dy = y1 - y0;
-    
+    for (let i = 2; i < coordinates.length; i++) {
+        const [x, y] = coordinates[i];
+        if ((x - x0) * dy !== (y - y0) * dx) {
+            return false;
+        }
+    }
+    return true;
 };
 // @lc code=end
