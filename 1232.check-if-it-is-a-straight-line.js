88ae9cea9ac0fad08c20add123463fc2a222926b
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-05-25 12:27:38 -0700

    working on checkStraightLine

---
+++ 1232.check-if-it-is-a-straight-line.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=1232 lang=javascript
+ *
+ * [1232] Check If It Is a Straight Line
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} coordinates
+ * @return {boolean}
+ */
+var checkStraightLine = function(coordinates) {
+    const [x0, y0] = coordinates[0];
+    const [x1, y1] = coordinates[1];
+    const dx = x1 - x0;
+    const dy = y1 - y0;
+    
+};
+// @lc code=end
