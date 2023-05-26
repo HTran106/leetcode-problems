a298269cec2931c369a7875428845f12fade7a71
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-05-26 12:31:14 -0700

    working on shiftGrid

--- 1260.shift-2-d-grid.js
+++ 1260.shift-2-d-grid.js
@@ -14,6 +14,13 @@ var shiftGrid = function(grid, k) {
     const m = grid.length;
     const n = grid[0].length;
     const res = new Array(m).fill(0).map(() => new Array(n).fill(0));
-    
+    for (let i = 0; i < m; i++) {
+        for (let j = 0;j < n; j++) {
+            const newJ = (j + k) % n;
+            const newI = (i + Math.floor((j + k) / n)) % m;
+            res[newI][newJ] = grid[i][j];
+        }
+    }
+
 };
 // @lc code=end
