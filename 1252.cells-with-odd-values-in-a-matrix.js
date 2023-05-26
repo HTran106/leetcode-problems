5ab8a494493da9502ad8d2974409889868dca787
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-05-26 12:29:57 -0700

    working on oddCells

---
+++ 1252.cells-with-odd-values-in-a-matrix.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=1252 lang=javascript
+ *
+ * [1252] Cells with Odd Values in a Matrix
+ */
+
+// @lc code=start
+/**
+ * @param {number} m
+ * @param {number} n
+ * @param {number[][]} indices
+ * @return {number}
+ */
+var oddCells = function(m, n, indices) {
+    const row = new Array(m).fill(0);
+    const col = new Array(n).fill(0);
+    
+};
+// @lc code=end
