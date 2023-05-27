7d1948134dab95c42b7c0572c5dadb119da2e476
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-05-27 12:32:14 -0700

    working on minTimeToVisitAllPoints

---
+++ 1266.minimum-time-visiting-all-points.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=1266 lang=javascript
+ *
+ * [1266] Minimum Time Visiting All Points
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var minTimeToVisitAllPoints = function(points) {
+    const n = points.length;
+    let res = 0;
+    for (let i = 1; i < n; i++) {
+        const dx = Math.abs(points[i][0] - points[i - 1][0]);
+        const dy = Math.abs(points[i][1] - points[i - 1][1]);
+        res += Math.max(dx, dy);
+    }
+};
+// @lc code=end
