b5d4d00a858ebfa33f39864442d69c7062ffdfc1
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-05-29 08:38:01 -0700

    done with numberOf1Bits

--- 191.number-of-1-bits.js
+++ 191.number-of-1-bits.js
@@ -12,6 +12,9 @@
 var hammingWeight = function(n) {
     const str = n.toString(2)
     let count = 0
-    
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === '1') count++
+    }
+    return count
 };
 // @lc code=end
