5b24e6325be86e6108eef60ac192e0c7af5a56a6
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-01 08:44:26 -0700

    woorking on maxCount

---
+++ 598.range-addition-ii.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=598 lang=javascript
+ *
+ * [598] Range Addition II
+ */
+
+// @lc code=start
+/**
+ * @param {number} m
+ * @param {number} n
+ * @param {number[][]} ops
+ * @return {number}
+ */
+var maxCount = function(m, n, ops) {
+    const result = []
+    for (let i = 0; i < m; i++) {
+        result.push(new Array(n).fill(0))
+    }
+    
+};
+// @lc code=end
