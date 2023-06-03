248c9cacb30631acd888aafba922d50d24a1c06d
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-03 08:49:40 -0700

    working on validPalindrome2

--- 680.valid-palindrome-ii.js
+++ 680.valid-palindrome-ii.js
@@ -20,5 +20,13 @@ var validPalindrome = function(s) {
         return true
     }
     let i = 0
+    let j = s.length - 1
+    while (i < j) {
+        if (s[i] !== s[j]) {
+            return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1)
+        }
+        i++
+        j--
+    }
 };
 // @lc code=end
