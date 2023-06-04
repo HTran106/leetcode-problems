3183c43a44ef44df214a2220d76c11320d80dc77
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-04 16:22:10 -0700

    working on floodFill

--- 733.flood-fill.js
+++ 733.flood-fill.js
@@ -14,6 +14,16 @@
  */
 var floodFill = function(image, sr, sc, color) {
     const oldColor = image[sr][sc];
+
     if (oldColor === color) return image;
+
+    const dfs = (r, c) => {
+        if (r < 0 || c < 0 || r >= image.length || c >= image[0].length || image[r][c] !== oldColor) return;
+        image[r][c] = color;
+        dfs(r + 1, c);
+        dfs(r - 1, c);
+        dfs(r, c + 1);
+        dfs(r, c - 1);
+    }
 };
 // @lc code=end
