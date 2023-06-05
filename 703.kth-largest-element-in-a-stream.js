71280e94bbe2c0228f24cbd42ec104a97448836a
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-05 08:52:28 -0700

    kth largest element

---
+++ 703.kth-largest-element-in-a-stream.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=703 lang=javascript
+ *
+ * [703] Kth Largest Element in a Stream
+ */
+
+// @lc code=start
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ */
+var KthLargest = function(k, nums) {
+
+};
+
+/**
+ * @param {number} val
+ * @return {number}
+ */
+KthLargest.prototype.add = function(val) {
+    const set = new Set()
+    let max = -Infinity
+    
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * var obj = new KthLargest(k, nums)
+ * var param_1 = obj.add(val)
+ */
+// @lc code=end
