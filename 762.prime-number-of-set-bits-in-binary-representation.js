c7b98c3c1747110ef8b1e67401092d663d57928a
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-06 16:23:22 -0700

    working on countPrimeSetBits

---
+++ 762.prime-number-of-set-bits-in-binary-representation.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=762 lang=javascript
+ *
+ * [762] Prime Number of Set Bits in Binary Representation
+ */
+
+// @lc code=start
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+var countPrimeSetBits = function(left, right) {
+    const isPrime = (num) => {
+        if (num === 1) return false;
+        if (num === 2) return true;
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) return false;
+        }
+        return true;
+    }
+    
+};
+// @lc code=end
