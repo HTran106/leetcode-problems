b8e6056b3724cf5351b81a4d4d8c9e3e40cb8782
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-08 16:26:09 -0700

    finished with surfaceArea

--- 892.surface-area-of-3-d-shapes.js
+++ 892.surface-area-of-3-d-shapes.js
@@ -12,6 +12,18 @@
 var surfaceArea = function(grid) {
     const row = grid.length;
     const col = grid[0].length;
-    
+    let area = 0;
+
+    for (let r = 0; r < row; r++) {
+        for (let c = 0; c < col; c ++) {
+            if (grid[r][c] > 0) {
+                area += grid[r][c] * 4 + 2;
+                if (r > 0) area -= Math.min(grid[r][c], grid[r - 1][c]) * 2;
+                if (c > 0) area -= Math.min(grid[r][c], grid[r][c - 1]) * 2;
+            }
+        }
+    }
+
+    return area;
 };
 // @lc code=end
