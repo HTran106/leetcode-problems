044942e5245818a909e9ad1cf496618798616d2a
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-08 16:25:30 -0700

    working on surfaceArea

---
+++ 892.surface-area-of-3-d-shapes.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=892 lang=javascript
+ *
+ * [892] Surface Area of 3D Shapes
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var surfaceArea = function(grid) {
+    const row = grid.length;
+    const col = grid[0].length;
+    
+};
+// @lc code=end
