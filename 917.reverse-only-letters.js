6497ecff498a95b4efec6e52b8a487d31202495e
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-09 16:27:27 -0700

    done with reverseOnlyLetters

--- 917.reverse-only-letters.js
+++ 917.reverse-only-letters.js
@@ -9,10 +9,31 @@
  * @param {string} s
  * @return {string}
  */
+const isLetter = (c) => {
+    const code = c.charCodeAt(0);
+    return (code >= 65 && code <= 90) || (code >= 97 && code <= 122);
+}
+
 var reverseOnlyLetters = function(s) {
     const arr = s.split('');
     let l = 0;
     let r = arr.length - 1;
-    
+
+    while (l < r) {
+        if (!isLetter(arr[l])) {
+            l++;
+            continue;
+        }
+        if (!isLetter(arr[r])) {
+            r--;
+            continue;
+        }
+
+        [arr[l], arr[r]] = [arr[r], arr[l]];
+        l++;
+        r--;
+    }
+
+    return arr.join('');
 };
 // @lc code=end
