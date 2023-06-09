d797e3abc539319c7525424235bbf3e09113ce34
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-09 16:26:38 -0700

    working on reverseOnlyLetters

--- 917.reverse-only-letters.js
+++ 917.reverse-only-letters.js
@@ -10,12 +10,9 @@
  * @return {string}
  */
 var reverseOnlyLetters = function(s) {
-    let alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    let arr = s.split('');
-    let res = [];
-    for (let i = arr.length - 1; i >= 0; i--) {
-            res.push(arr[i]);
-    }
-    return res.join('')
+    const arr = s.split('');
+    let l = 0;
+    let r = arr.length - 1;
+    
 };
 // @lc code=end
