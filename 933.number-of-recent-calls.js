a7d69e2084c4f2f7a5ee62f4d03341063fd411a5
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-10 16:31:13 -0700

    working on number of recent calls

--- 933.number-of-recent-calls.js
+++ 933.number-of-recent-calls.js
@@ -64,7 +64,62 @@ RecentCounter.prototype.ping = function(t) {
     }
 
     BST.prototype.bfs = function() {
-        
+        const data = [];
+        const queue = [];
+        let node = this.root;
+        queue.push(node);
+        while (queue.length) {
+            node = queue.shift();
+            data.push(node.val);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        return data;
+    }
+
+    BST.prototype.dfsPreOrder = function() {
+        const data = [];
+        function traverse(node) {
+            data.push(node.val);
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
+
+    BST.prototype.dfsPostOrder = function() {
+        const data = [];
+        function traverse(node) {
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+            data.push(node.val);
+        }
+        traverse(this.root);
+        return data;
+    }
+
+    BST.prototype.dfsInOrder = function() {
+        const data = [];
+        function traverse(node) {
+            if (node.left) traverse(node.left);
+            data.push(node.val);
+            if (node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
+
+    const bst = new BST();
+    bst.insert(t);
+
+    let count = 0;
+    const arr = bst.dfsInOrder();
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] >= t - 3000 && arr[i] <= t) count++;
+    }
+
+    return count;
 };
 
 /**
