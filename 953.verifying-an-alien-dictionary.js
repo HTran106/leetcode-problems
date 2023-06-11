bb3463f56198984c2fec4e83b92b7b6352e7e8e3
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-11 16:31:53 -0700

    working isAlienSorted

---
+++ 953.verifying-an-alien-dictionary.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=953 lang=javascript
+ *
+ * [953] Verifying an Alien Dictionary
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} words
+ * @param {string} order
+ * @return {boolean}
+ */
+var isAlienSorted = function(words, order) {
+    const map = {};
+    for (let i = 0; i < order.length; i++) {
+        map[order[i]] = i;
+    }
+
+    const compare = (a, b) => {
+        let i = 0;
+        let j = 0;
+        while (i < a.length && j < b.length) {
+            if (map[a[i]] < map[b[j]]) return -1;
+            if (map[a[i]] > map[b[j]]) return 1;
+            i++;
+            j++;
+        }
+        return a.length - b.length;
+    }
+};
+// @lc code=end
