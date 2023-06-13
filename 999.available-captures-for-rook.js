300dd1a9cee8ed878bb7f6733055d9739aef6c59
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-13 16:34:44 -0700

    working on numRookCaptures

--- 999.available-captures-for-rook.js
+++ 999.available-captures-for-rook.js
@@ -14,5 +14,17 @@ var numRookCaptures = function(board) {
     const col = board[0].length;
     let rookRow = 0;
     let rookCol = 0;
+
+    for (let r = 0; r < row; r++) {
+        for (let c = 0; c < col; c ++) {
+            if (board[r][c] === 'R') {
+                rookRow = r;
+                rookCol = c;
+                break;
+            }
+        }
+    }
+
+    
 };
 // @lc code=end
