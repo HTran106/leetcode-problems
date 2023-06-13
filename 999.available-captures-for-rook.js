f35158e4557b42e9c5f8f8e34bb907b8e589fe89
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-13 16:34:20 -0700

    working on numRookCaptures

---
+++ 999.available-captures-for-rook.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=999 lang=javascript
+ *
+ * [999] Available Captures for Rook
+ */
+
+// @lc code=start
+/**
+ * @param {character[][]} board
+ * @return {number}
+ */
+var numRookCaptures = function(board) {
+    
+};
+// @lc code=end
