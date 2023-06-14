65e43f18331bf578ea4eefd7636a72715fb35edf
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-14 16:36:04 -0700

    working on bitwiseComplement

---
+++ 1009.complement-of-base-10-integer.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1009 lang=javascript
+ *
+ * [1009] Complement of Base 10 Integer
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var bitwiseComplement = function(n) {
+    const binary = n.toString(2);
+    let result = '';
+    
+};
+// @lc code=end
