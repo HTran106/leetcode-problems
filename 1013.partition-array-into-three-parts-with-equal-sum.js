f9481ee27d20f1751cc246a4e0cc52daa0d14433
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-15 16:37:14 -0700

    done with canThreePartsEqualSum

--- 1013.partition-array-into-three-parts-with-equal-sum.js
+++ 1013.partition-array-into-three-parts-with-equal-sum.js
@@ -15,5 +15,13 @@ var canThreePartsEqualSum = function(arr) {
     const target = sum / 3;
     let count = 0;
     let cur = 0;
+    for (let i = 0; i < arr.length; i++) {
+        cur += arr[i];
+        if (cur === target) {
+            count++;
+            cur = 0;
+        }
+    }
+    return count >= 3;
 };
 // @lc code=end
