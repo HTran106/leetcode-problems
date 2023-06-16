bc547c3e8f1ca8a497c96fae5c762dffd044e0b4
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-16 16:38:33 -0700

    done with removeOuterParentheses

--- 1021.remove-outermost-parentheses.js
+++ 1021.remove-outermost-parentheses.js
@@ -11,6 +11,16 @@
  */
 var removeOuterParentheses = function(s) {
     const stack = [];
-    
+    let result = '';
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') {
+            if (stack.length) result += s[i];
+            stack.push(s[i]);
+        } else {
+            stack.pop();
+            if (stack.length) result += s[i];
+        }
+    }
+    return result;
 };
 // @lc code=end
