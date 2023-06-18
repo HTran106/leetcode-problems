6eb17ad475479c52c25891dff1f55d38970bc015
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-18 16:41:18 -0700

    working on gcdOfStrings

---
+++ 1071.greatest-common-divisor-of-strings.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1071 lang=javascript
+ *
+ * [1071] Greatest Common Divisor of Strings
+ */
+
+// @lc code=start
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function(str1, str2) {
+    
+};
+// @lc code=end
+
