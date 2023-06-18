e9514afe72375ede58e544e9a99aedf82bf7a5fd
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-18 16:43:14 -0700

    done with findOcurrences

--- 1078.occurrences-after-bigram.js
+++ 1078.occurrences-after-bigram.js
@@ -14,6 +14,13 @@
 var findOcurrences = function(text, first, second) {
     const result = [];
     const words = text.split(' ');
-    
+
+    for (let i = 0; i < words.length - 2; i++) {
+        if (words[i] === first && words[i + 1] === second) {
+            result.push(words[i + 2]);
+        }
+    }
+
+    return result;
 };
 // @lc code=end
