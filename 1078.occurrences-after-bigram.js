1421ab641a23a2b501301db6743d43eb71e801cf
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-18 16:42:52 -0700

    working on findOcurrences

---
+++ 1078.occurrences-after-bigram.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=1078 lang=javascript
+ *
+ * [1078] Occurrences After Bigram
+ */
+
+// @lc code=start
+/**
+ * @param {string} text
+ * @param {string} first
+ * @param {string} second
+ * @return {string[]}
+ */
+var findOcurrences = function(text, first, second) {
+    const result = [];
+    const words = text.split(' ');
+    
+};
+// @lc code=end
