dfbaa935b4f7476848510349eab0029fc1207127
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-18 16:40:17 -0700

    done with isBoomerang

--- 1037.valid-boomerang.js
+++ 1037.valid-boomerang.js
@@ -16,6 +16,8 @@ var isBoomerang = function(points) {
     if (x1 === x2 && y1 === y2) return false;
 
     const [x3, y3] = points[2];
+
+    return (y2 - y1) * (x3 - x2) !== (y3 - y2) * (x2 - x1);
     
 };
 // @lc code=end
