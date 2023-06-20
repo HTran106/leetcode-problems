bff8c1ee939bf9c4bd2d50dea55cdcb33a6c4e5e
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-20 16:46:00 -0700

    working on ticTacToe

---
+++ 1275.find-winner-on-a-tic-tac-toe-game.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1275 lang=javascript
+ *
+ * [1275] Find Winner on a Tic Tac Toe Game
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} moves
+ * @return {string}
+ */
+var tictactoe = function(moves) {
+    const board = [[], [], []];
+};
+// @lc code=end
