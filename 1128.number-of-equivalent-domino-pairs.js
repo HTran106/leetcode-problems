af554c5e3ad5a19e692e36c17e03da07922025dc
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-20 16:45:06 -0700

    working on numEquivDominoPairs

---
+++ 1128.number-of-equivalent-domino-pairs.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1128 lang=javascript
+ *
+ * [1128] Number of Equivalent Domino Pairs
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} dominoes
+ * @return {number}
+ */
+var numEquivDominoPairs = function(dominoes) {
+    const map = {};
+    let count = 0;
+    
+};
+// @lc code=end
