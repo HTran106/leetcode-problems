c3029f80e8373a0a227b6832c313848f9072b040
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-21 16:47:38 -0700

    working on ticTacToe

--- 1275.find-winner-on-a-tic-tac-toe-game.js
+++ 1275.find-winner-on-a-tic-tac-toe-game.js
@@ -10,11 +10,6 @@
  * @return {string}
  */
 var tictactoe = function(moves) {
-    const board = [[], [], []];
-    for (let i = 0; i < moves.length; i++) {
-        const [x, y] = moves[i];
-        board[x][y] = i % 2 ? 'O' : 'X';
-    }
     
 };
 // @lc code=end
