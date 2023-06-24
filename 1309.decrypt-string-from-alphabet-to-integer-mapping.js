e0778bf8143e0d2e1a91034d5f3ef518d998382b
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-23 17:02:33 -0700

    done with freqAlphabets

--- 1309.decrypt-string-from-alphabet-to-integer-mapping.js
+++ 1309.decrypt-string-from-alphabet-to-integer-mapping.js
@@ -14,6 +14,17 @@ var freqAlphabets = function(s) {
     for (let i = 1; i <= 26; i++) {
         map.set(i.toString(), String.fromCharCode(96 + i));
     }
-    
+    const result = [];
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i + 2] === '#') {
+            result.push(map.get(s[i] + s[i + 1]));
+            i += 2;
+        } else {
+            result.push(map.get(s[i]));
+        }
+    }
+
+    return result.join('');
 };
 // @lc code=end
