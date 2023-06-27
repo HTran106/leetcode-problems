21d62755f89c5f044ea4faa8f08c65006ab1dd26
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-26 17:06:05 -0700

    working on checkIfExist

---
+++ 1346.check-if-n-and-its-double-exist.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1346 lang=javascript
+ *
+ * [1346] Check If N and Its Double Exist
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var checkIfExist = function(arr) {
+    const map = {};
+    
+};
+// @lc code=end
