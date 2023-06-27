866d75afff7d0f03e51bb6f1e9f31957acacfec9
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-26 17:06:28 -0700

    done with checkifExist

--- 1346.check-if-n-and-its-double-exist.js
+++ 1346.check-if-n-and-its-double-exist.js
@@ -11,6 +11,14 @@
  */
 var checkIfExist = function(arr) {
     const map = {};
-    
+
+    for (let i = 0; i < arr.length; i++) {
+        const num = arr[i];
+        if (map[num * 2] || map[num / 2]) {
+            return true;
+        }
+        map[num] = true;
+    }
+    return false;
 };
 // @lc code=end
