aad4352d83d066160c80b696071fc9399d188c72
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-27 17:06:58 -0700

    working on countNegatives

---
+++ 1351.count-negative-numbers-in-a-sorted-matrix.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1351 lang=javascript
+ *
+ * [1351] Count Negative Numbers in a Sorted Matrix
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var countNegatives = function(grid) {
+    const m = grid.length;
+    const n = grid[0].length;
+    
+};
+// @lc code=end
