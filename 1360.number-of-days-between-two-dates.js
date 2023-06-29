3d14d66f9d8f2b11bd55564760ecb3bbb5019e51
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-28 17:08:51 -0700

    working on daysBetweenDates

---
+++ 1360.number-of-days-between-two-dates.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1360 lang=javascript
+ *
+ * [1360] Number of Days Between Two Dates
+ */
+
+// @lc code=start
+/**
+ * @param {string} date1
+ * @param {string} date2
+ * @return {number}
+ */
+var daysBetweenDates = function(date1, date2) {
+    const date1Obj = new Date(date1);
+    const date2Obj = new Date(date2);
+};
+// @lc code=end
