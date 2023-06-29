0a8b0bf34ca9899b0c72c3389780a511efa6223d
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-28 17:09:47 -0700

    working on sortString

---
+++ 1365.how-many-numbers-are-smaller-than-the-current-number.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1365 lang=javascript
+ *
+ * [1365] How Many Numbers Are Smaller Than the Current Number
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var smallerNumbersThanCurrent = function(nums) {
+    
+};
+// @lc code=end
+
