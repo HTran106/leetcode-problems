648f30f9d2fb73bd99fde69238d105dd51b74439
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-28 17:10:39 -0700

    done with sortString

--- 1370.increasing-decreasing-string.js
+++ 1370.increasing-decreasing-string.js
@@ -17,6 +17,21 @@ var sortString = function(s) {
     }
     const sortedKeys = [...map.keys()].sort();
 
-    
+    while (result.length < s.length) {
+        for (let i = 0; i < sortedKeys.length; i++) {
+            if (map.get(sortedKeys[i]) > 0) {
+                result += sortedKeys[i];
+                map.set(sortedKeys[i], map.get(sortedKeys[i]) - 1);
+            }
+        }
+        for (let i = sortedKeys.length - 1; i >= 0; i--) {
+            if (map.get(sortedKeys[i]) > 0) {
+                result += sortedKeys[i];
+                map.set(sortedKeys[i], map.get(sortedKeys[i]) - 1);
+            }
+        }
+    }
+
+    return result;
 };
 // @lc code=end
