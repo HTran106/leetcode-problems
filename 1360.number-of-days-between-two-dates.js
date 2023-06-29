e04e1894f9159975ac7ce467164034ccd25d1cd5
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-28 17:09:11 -0700

    done with daysBetweenDates

--- 1360.number-of-days-between-two-dates.js
+++ 1360.number-of-days-between-two-dates.js
@@ -10,8 +10,10 @@
  * @param {string} date2
  * @return {number}
  */
-var daysBetweenDates = function(date1, date2) {
+var daysBetweenDates = function (date1, date2) {
     const date1Obj = new Date(date1);
     const date2Obj = new Date(date2);
+    const diff = Math.abs(date1Obj - date2Obj);
+    return diff / (1000 * 60 * 60 * 24);
 };
 // @lc code=end
