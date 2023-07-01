62543ba826d1265cafc927507a0209fd7c7cdba1
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-30 17:12:27 -0700

    working with luckyNumbers

--- 1380.lucky-numbers-in-a-matrix.js
+++ 1380.lucky-numbers-in-a-matrix.js
@@ -12,5 +12,25 @@
 var luckyNumbers  = function(matrix) {
     const m = matrix.length;
     const n = matrix[0].length;
+
+    const minRow = [];
+    const maxCol = [];
+
+    for (let i = 0; i < m; i++) {
+        let min = Infinity;
+        for (let j = 0; j < n; j++) {
+            min = Math.min(min, matrix[i][j]);
+        }
+        minRow.push(min);
+    }
+
+    for (let j = 0; j < n; j++) {
+        let max = -Infinity;
+        for (let i = 0; i < m; i++) {
+            max = Math.max(max, matrix[i][j]);
+        }
+        maxCol.push(max);
+    }
+    
 };
 // @lc code=end
