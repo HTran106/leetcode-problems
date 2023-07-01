52d3e4211f6599ed763c368b9c4cdc7c1657c4b4
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-06-30 17:12:12 -0700

    working with luckyNumbers

---
+++ 1380.lucky-numbers-in-a-matrix.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1380 lang=javascript
+ *
+ * [1380] Lucky Numbers in a Matrix
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var luckyNumbers  = function(matrix) {
+    const m = matrix.length;
+    const n = matrix[0].length;
+};
+// @lc code=end
