19cedf7eea2449dbc42746faf26ec2fceeef3314
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-01 17:14:03 -0700

    done with createTargetArray

--- 1389.create-target-array-in-the-given-order.js
+++ 1389.create-target-array-in-the-given-order.js
@@ -12,5 +12,9 @@
  */
 var createTargetArray = function(nums, index) {
     const result = [];
+    for (let i = 0; i < nums.length; i++) {
+        result.splice(index[i], 0, nums[i]);
+    }
+    return result;
 };
 // @lc code=end
