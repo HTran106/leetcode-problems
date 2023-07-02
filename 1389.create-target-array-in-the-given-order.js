cc1fc1ab212c102a3965dbbf7804a20f6c574b05
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-01 17:13:42 -0700

    working on createTargetArray

---
+++ 1389.create-target-array-in-the-given-order.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1389 lang=javascript
+ *
+ * [1389] Create Target Array in the Given Order
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number[]} index
+ * @return {number[]}
+ */
+var createTargetArray = function(nums, index) {
+    const result = [];
+};
+// @lc code=end
