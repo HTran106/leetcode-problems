81b0e58f0e98cdcc840dd202f33868c70ff0c0d8
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-03 17:17:36 -0700

    done with minSubsequence

--- 1403.minimum-subsequence-in-non-increasing-order.js
+++ 1403.minimum-subsequence-in-non-increasing-order.js
@@ -12,6 +12,15 @@
 var minSubsequence = function(nums) {
     const sorted = nums.sort((a, b) => b - a);
     const sum = sorted.reduce((acc, cur) => acc + cur, 0);
-    
+    let subSum = 0;
+    const result = [];
+    for (let i = 0; i < sorted.length; i++) {
+        subSum += sorted[i];
+        result.push(sorted[i]);
+        if (subSum > sum - subSum) {
+            break;
+        }
+    }
+    return result;
 };
 // @lc code=end
