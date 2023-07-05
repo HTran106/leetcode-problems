4966fe2f9d42b45946a70a31a0d63b529e413f6b
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-04 17:20:12 -0700

    done with stringMatching

--- 1408.string-matching-in-an-array.js
+++ 1408.string-matching-in-an-array.js
@@ -9,13 +9,16 @@
  * @param {string[]} words
  * @return {string[]}
  */
-var stringMatching = function(words) {
+var stringMatching = function (words) {
+    if (words.length === 0) {
+        return [];
+    }
     const result = [];
     const map = new Map();
     for (let i = 0; i < words.length; i++) {
         map.set(words[i], (map.get(words[i]) || 0) + 1);
     }
-    const sortedKeys = [...map.keys()].sort((a, b) => b.length - a.length);
+    const sortedKeys = [...map.keys()].sort((a, b) => a.length - b.length);
 
     for (let i = 0; i < sortedKeys.length; i++) {
         for (let j = i + 1; j < sortedKeys.length; j++) {
