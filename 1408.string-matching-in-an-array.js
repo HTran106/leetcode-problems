ef055c5836fdde6d03fe52d4e0daf43169d11bee
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-04 17:18:38 -0700

    working on stringMatching

---
+++ 1408.string-matching-in-an-array.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1408 lang=javascript
+ *
+ * [1408] String Matching in an Array
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var stringMatching = function(words) {
+    const result = [];
+    
+};
+// @lc code=end
