9147c583a749fadc38b59d05b4febc682e11cbb2
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-05 17:21:22 -0700

    working on reformat

---
+++ 1417.reformat-the-string.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1417 lang=javascript
+ *
+ * [1417] Reformat The String
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reformat = function(s) {
+    const letters = [];
+    const numbers = [];
+    
+};
+// @lc code=end
