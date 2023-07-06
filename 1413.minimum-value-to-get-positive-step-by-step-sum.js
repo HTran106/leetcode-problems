c38a3248c753501c2206034b6daf3c758f09f88f
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-05 17:21:05 -0700

    done with minStartValue

--- 1413.minimum-value-to-get-positive-step-by-step-sum.js
+++ 1413.minimum-value-to-get-positive-step-by-step-sum.js
@@ -13,6 +13,10 @@ var minStartValue = function(nums) {
     const sum = nums.reduce((acc, cur) => acc + cur, 0);
     let min = sum;
     let current = 0;
-    
+    for (let i = 0; i < nums.length; i++) {
+        current += nums[i];
+        min = Math.min(min, current);
+    }
+    return min < 0 ? 1 - min : 1;
 };
 // @lc code=end
