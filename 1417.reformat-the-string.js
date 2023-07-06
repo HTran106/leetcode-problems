bc1f3eff75dbc4bb6ff55f251c9cf5dc245edd9d
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-05 17:21:33 -0700

    working on reformat

--- 1417.reformat-the-string.js
+++ 1417.reformat-the-string.js
@@ -12,6 +12,18 @@
 var reformat = function(s) {
     const letters = [];
     const numbers = [];
+
+    for (let i = 0; i < s.length; i++) {
+        if (isNaN(s[i])) {
+            letters.push(s[i]);
+        } else {
+            numbers.push(s[i]);
+        }
+    }
+
+    if (Math.abs(letters.length - numbers.length) > 1) {
+        return '';
+    }
     
 };
 // @lc code=end
