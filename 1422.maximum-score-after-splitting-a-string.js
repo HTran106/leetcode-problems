ca53d9e7fb669564b0dc1649f03424f38cb42678
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-06 17:22:41 -0700

    maxScore

---
+++ 1422.maximum-score-after-splitting-a-string.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1422 lang=javascript
+ *
+ * [1422] Maximum Score After Splitting a String
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxScore = function(s) {
+    const left = new Array(s.length).fill(0);
+    const right = new Array(s.length).fill(0);
+    
+};
+// @lc code=end
