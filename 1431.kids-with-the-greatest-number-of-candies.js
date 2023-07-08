1706d2d744eaed03cc8c857bca367dced523fd4b
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-07 17:23:46 -0700

    working on kidsWithCandies

---
+++ 1431.kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1431 lang=javascript
+ *
+ * [1431] Kids With the Greatest Number of Candies
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    const max = Math.max(...candies);
+};
+// @lc code=end
