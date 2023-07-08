d2daafd19414d53b792851a705b35740ee5810b4
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-07 17:24:08 -0700

    done with kidsWithCandies

--- 1431.kids-with-the-greatest-number-of-candies.js
+++ 1431.kids-with-the-greatest-number-of-candies.js
@@ -12,5 +12,10 @@
  */
 var kidsWithCandies = function(candies, extraCandies) {
     const max = Math.max(...candies);
+    const result = [];
+    for (let i = 0; i < candies.length; i++) {
+        result.push(candies[i] + extraCandies >= max);
+    }
+    return result;
 };
 // @lc code=end
