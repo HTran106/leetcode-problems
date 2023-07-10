80d590eb8af259240dbbc19aaaade8a8a2911a68
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-09 17:26:40 -0700

    working on busyStudent

---
+++ 1450.number-of-students-doing-homework-at-a-given-time.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=1450 lang=javascript
+ *
+ * [1450] Number of Students Doing Homework at a Given Time
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} startTime
+ * @param {number[]} endTime
+ * @param {number} queryTime
+ * @return {number}
+ */
+var busyStudent = function(startTime, endTime, queryTime) {
+    const result = [];
+    
+};
+// @lc code=end
