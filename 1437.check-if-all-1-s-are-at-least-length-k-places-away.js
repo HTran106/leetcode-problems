302fe32945e77ddf5c05d70f32e40a6a126f61d7
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-09 17:25:15 -0700

    done with destCity

---
+++ 1437.check-if-all-1-s-are-at-least-length-k-places-away.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1437 lang=javascript
+ *
+ * [1437] Check If All 1's Are at Least Length K Places Away
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var kLengthApart = function(nums, k) {
+    const ones = [];
+    
+};
+// @lc code=end
