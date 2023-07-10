66d890cf9f6d0d76a7ba7afe69b3b4c4903a9dae
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-09 17:25:54 -0700

    working on maxPower

---
+++ 1446.consecutive-characters.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1446 lang=javascript
+ *
+ * [1446] Consecutive Characters
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxPower = function(s) {
+    const result = [];
+};
+// @lc code=end
