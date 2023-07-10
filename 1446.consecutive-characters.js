c7fb59c1fbd76e1060dadc95fd32692390b1c144
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-09 17:26:06 -0700

    working on maxPower

--- 1446.consecutive-characters.js
+++ 1446.consecutive-characters.js
@@ -11,5 +11,18 @@
  */
 var maxPower = function(s) {
     const result = [];
+    let current = s[0];
+    let count = 1;
+    for (let i = 1; i < s.length; i++) {
+        if (s[i] === current) {
+            count++;
+        } else {
+            result.push(count);
+            current = s[i];
+            count = 1;
+        }
+    }
+    result.push(count);
+    
 };
 // @lc code=end
