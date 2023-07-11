872d6fcd242e127b8b39ae98af559d50148d65ca
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-10 17:29:47 -0700

    working on isPrefixOfWord

---
+++ 1455.check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1455 lang=javascript
+ *
+ * [1455] Check If a Word Occurs As a Prefix of Any Word in a Sentence
+ */
+
+// @lc code=start
+/**
+ * @param {string} sentence
+ * @param {string} searchWord
+ * @return {number}
+ */
+var isPrefixOfWord = function(sentence, searchWord) {
+    const words = sentence.split(' ');
+    
+};
+// @lc code=end
