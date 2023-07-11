12992ab1ef9f35c52d891fd3d9f1d414977ee871
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-10 17:30:06 -0700

    done with isPrefixOfWord

--- 1455.check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.js
+++ 1455.check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.js
@@ -12,6 +12,11 @@
  */
 var isPrefixOfWord = function(sentence, searchWord) {
     const words = sentence.split(' ');
-    
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].startsWith(searchWord)) {
+            return i + 1;
+        }
+    }
+    return -1;
 };
 // @lc code=end
