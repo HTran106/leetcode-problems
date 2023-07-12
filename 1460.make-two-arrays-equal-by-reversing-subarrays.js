fa6273b96eeb9f88ee5918094609efb50d63229e
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-11 17:30:34 -0700

    working on canBeEqual

---
+++ 1460.make-two-arrays-equal-by-reversing-subarrays.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1460 lang=javascript
+ *
+ * [1460] Make Two Arrays Equal by Reversing Subarrays
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} target
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var canBeEqual = function(target, arr) {
+    const map = {};
+    
+};
+// @lc code=end
