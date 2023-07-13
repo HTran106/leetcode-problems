4c2ec0ace792fdef82b243b21b0afc88d426d0f0
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-12 17:31:58 -0700

    working on maxProduct

---
+++ 1464.maximum-product-of-two-elements-in-an-array.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1464 lang=javascript
+ *
+ * [1464] Maximum Product of Two Elements in an Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+    const sorted = nums.sort((a, b) => b - a);
+    
+};
+// @lc code=end
