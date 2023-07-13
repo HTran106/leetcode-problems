b75a74ff4f183c69a5efe5092596dffbf2d0a6fe
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-12 17:33:14 -0700

    done with shuffle

--- 1470.shuffle-the-array.js
+++ 1470.shuffle-the-array.js
@@ -12,6 +12,9 @@
  */
 var shuffle = function(nums, n) {
     const result = [];
-    
+    for (let i = 0; i < n; i++) {
+        result.push(nums[i], nums[i + n]);
+    }
+    return result;
 };
 // @lc code=end
