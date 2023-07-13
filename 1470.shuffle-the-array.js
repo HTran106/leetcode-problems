40940a74b28fe7a15074f17ca7ddc9cb475a9d89
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-12 17:32:57 -0700

    working on shuffle

---
+++ 1470.shuffle-the-array.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1470 lang=javascript
+ *
+ * [1470] Shuffle the Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+    const result = [];
+    
+};
+// @lc code=end
