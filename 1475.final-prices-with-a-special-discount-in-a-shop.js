ff20ffc6861e353a6df932e0be64fda3a7f56158
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-13 17:33:46 -0700

    working on finalPrices

---
+++ 1475.final-prices-with-a-special-discount-in-a-shop.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=1475 lang=javascript
+ *
+ * [1475] Final Prices With a Special Discount in a Shop
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} prices
+ * @return {number[]}
+ */
+var finalPrices = function(prices) {
+    const result = [];
+    for (let i = 0; i < prices.length; i++) {
+        let discount = 0;
+        for (let j = i + 1; j < prices.length; j++) {
+            if (prices[j] <= prices[i]) {
+                discount = prices[j];
+                break;
+            }
+        }
+        result.push(prices[i] - discount);
+    }
+};
+// @lc code=end
