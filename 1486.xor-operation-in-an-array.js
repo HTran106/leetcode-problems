0ab18c67c576f999ab71041d86cf6a2b639faa20
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-14 17:34:35 -0700

    working on xorOperation

---
+++ 1486.xor-operation-in-an-array.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1486 lang=javascript
+ *
+ * [1486] XOR Operation in an Array
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @param {number} start
+ * @return {number}
+ */
+var xorOperation = function(n, start) {
+    const nums = [];
+    
+};
+// @lc code=end
