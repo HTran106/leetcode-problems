6de6bef70070e49eaecf305066321146be826993
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-15 17:36:53 -0700

    done with isPathCrossing

--- 1496.path-crossing.js
+++ 1496.path-crossing.js
@@ -15,6 +15,29 @@ var isPathCrossing = function(path) {
     let y = 0;
     map[`${x},${y}`] = true;
 
-    
+    for (let i = 0; i < path.length; i++) {
+        const direction = path[i];
+        switch (direction) {
+            case 'N':
+                y++;
+                break;
+            case 'S':
+                y--;
+                break;
+            case 'E':
+                x++;
+                break;
+            case 'W':
+                x--;
+                break;
+        }
+        const key = `${x},${y}`;
+        if (map[key]) {
+            return true;
+        }
+        map[key] = true;
+    }
+
+    return false;
 };
 // @lc code=end
