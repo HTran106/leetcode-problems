49f1a9a198fd233f31f42a2543b16ec8d3786cc8
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-15 17:35:21 -0700

    working on average

---
+++ 1491.average-salary-excluding-the-minimum-and-maximum-salary.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1491 lang=javascript
+ *
+ * [1491] Average Salary Excluding the Minimum and Maximum Salary
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} salary
+ * @return {number}
+ */
+var average = function(salary) {
+    const sorted = salary.sort((a, b) => a - b);
+    
+};
+// @lc code=end
