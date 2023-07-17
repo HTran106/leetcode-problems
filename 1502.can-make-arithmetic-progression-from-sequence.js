7502982fa16811b53d07552150b68e1a07128884
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-16 17:37:22 -0700

    working on canMakeArithmeticProgression

---
+++ 1502.can-make-arithmetic-progression-from-sequence.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1502 lang=javascript
+ *
+ * [1502] Can Make Arithmetic Progression From Sequence
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var canMakeArithmeticProgression = function(arr) {
+    const sorted = arr.sort((a, b) => a - b);
+    
+};
+// @lc code=end
