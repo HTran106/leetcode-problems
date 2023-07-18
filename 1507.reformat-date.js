c45fca5fb1ad27249e47e8e5eec0a5284bf84245
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-17 17:38:21 -0700

    working on reformateDate

---
+++ 1507.reformat-date.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=1507 lang=javascript
+ *
+ * [1507] Reformat Date
+ */
+
+// @lc code=start
+/**
+ * @param {string} date
+ * @return {string}
+ */
+var reformatDate = function(date) {
+    const months = {
+        Jan: '01',
+        Feb: '02',
+        Mar: '03',
+        Apr: '04',
+        May: '05',
+        Jun: '06',
+        Jul: '07',
+        Aug: '08',
+        Sep: '09',
+        Oct: '10',
+        Nov: '11',
+        Dec: '12',
+    };
+};
+// @lc code=end
