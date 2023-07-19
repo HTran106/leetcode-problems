7209e154ad8bceb572525d980c695fc995c0badb
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-18 17:39:14 -0700

    working on numIdenticalPairs

---
+++ 1512.number-of-good-pairs.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1512 lang=javascript
+ *
+ * [1512] Number of Good Pairs
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function(nums) {
+    const map = new Map();
+    
+};
+// @lc code=end
