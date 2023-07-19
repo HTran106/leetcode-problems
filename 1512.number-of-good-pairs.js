13c5b6e7a833e970949acbadfc15d34e78996ef2
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-18 17:39:34 -0700

    done with numIdenticalPairs

--- 1512.number-of-good-pairs.js
+++ 1512.number-of-good-pairs.js
@@ -11,6 +11,15 @@
  */
 var numIdenticalPairs = function(nums) {
     const map = new Map();
-    
+    let count = 0;
+    for (let num of nums) {
+        if (map.has(num)) {
+            count += map.get(num);
+            map.set(num, map.get(num) + 1);
+        } else {
+            map.set(num, 1);
+        }
+    }
+    return count;
 };
 // @lc code=end
