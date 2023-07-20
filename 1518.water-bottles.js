dd1d55893f477b97470797397984a1bbc6e05489
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-19 17:40:07 -0700

    working on numWaterBottles

---
+++ 1518.water-bottles.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=1518 lang=javascript
+ *
+ * [1518] Water Bottles
+ */
+
+// @lc code=start
+/**
+ * @param {number} numBottles
+ * @param {number} numExchange
+ * @return {number}
+ */
+var numWaterBottles = function(numBottles, numExchange) {
+    const emptyBottles = numBottles;
+    let total = numBottles;
+    
+};
+// @lc code=end
