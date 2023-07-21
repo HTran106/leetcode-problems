0a534332ef19ddb45c46139697370ee2d6670fed
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-20 17:41:06 -0700

    working on restoreString

---
+++ 1528.shuffle-string.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=1528 lang=javascript
+ *
+ * [1528] Shuffle String
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function(s, indices) {
+    const chars = [];
+    for (let i = 0; i < s.length; i++) {
+        chars[indices[i]] = s[i];
+    }
+};
+// @lc code=end
