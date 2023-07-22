2e22bf2552c8e4f1d80e7db3dce5fe614617b264
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-21 17:42:11 -0700

    working on countGoodTriplets

--- 1534.count-good-triplets.js
+++ 1534.count-good-triplets.js
@@ -14,5 +14,18 @@
  */
 var countGoodTriplets = function(arr, a, b, c) {
     const n = arr.length;
+    let count = 0;
+
+    for (let i = 0; i < n - 2; i++) {
+        for (let j = i + 1; j < n - 1; j++) {
+            for (let k = j + 1; k < n; k++) {
+                if (Math.abs(arr[i] - arr[j]) <= a
+                    && Math.abs(arr[j] - arr[k]) <= b
+                    && Math.abs(arr[i] - arr[k]) <= c) {
+                    count++;
+                }
+            }
+        }
+    }
 };
 // @lc code=end
