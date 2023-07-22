b736aeb0f0c3ea77a5b5f76af209aecfc79615b3
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-21 17:41:54 -0700

    working on countGoodTriplets

---
+++ 1534.count-good-triplets.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=1534 lang=javascript
+ *
+ * [1534] Count Good Triplets
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} arr
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number}
+ */
+var countGoodTriplets = function(arr, a, b, c) {
+    const n = arr.length;
+};
+// @lc code=end
