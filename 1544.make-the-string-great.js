435dd511492828364bc5bf4c3da26837f6c8b059
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-23 17:44:08 -0700

    done with makeGood

--- 1544.make-the-string-great.js
+++ 1544.make-the-string-great.js
@@ -11,6 +11,13 @@
  */
 var makeGood = function(s) {
     const stack = [];
-    
+    for (let i = 0; i < s.length; i++) {
+        if (stack.length > 0 && Math.abs(stack[stack.length - 1].charCodeAt(0) - s[i].charCodeAt(0)) === 32) {
+            stack.pop();
+        } else {
+            stack.push(s[i]);
+        }
+    }
+    return stack.join('');
 };
 // @lc code=end
