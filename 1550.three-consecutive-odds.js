613b147519ede9119a859275ec806aa537063a37
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-24 17:44:56 -0700

    done wtih threeConsecutiveOdds

--- 1550.three-consecutive-odds.js
+++ 1550.three-consecutive-odds.js
@@ -14,6 +14,11 @@ var threeConsecutiveOdds = function(arr) {
     if (len < 3) {
         return false;
     }
-    
+    for (let i = 0; i < len - 2; i++) {
+        if (arr[i] % 2 === 1 && arr[i + 1] % 2 === 1 && arr[i + 2] % 2 === 1) {
+            return true;
+        }
+    }
+    return false;
 };
 // @lc code=end
