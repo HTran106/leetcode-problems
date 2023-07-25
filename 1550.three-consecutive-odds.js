5f194ceffb1beb18fc60052f94140b08fe52b41e
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-24 17:44:35 -0700

    working on threeConsecutiveOdds

---
+++ 1550.three-consecutive-odds.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=1550 lang=javascript
+ *
+ * [1550] Three Consecutive Odds
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var threeConsecutiveOdds = function(arr) {
+    const len = arr.length;
+    if (len < 3) {
+        return false;
+    }
+    
+};
+// @lc code=end
