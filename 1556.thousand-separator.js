69cc03b5618a8cd43335b445fb3d311cdff38b3e
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-25 17:45:22 -0700

    working on thousandSeperator

---
+++ 1556.thousand-separator.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1556 lang=javascript
+ *
+ * [1556] Thousand Separator
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var thousandSeparator = function(n) {
+    const str = n.toString();
+};
+// @lc code=end
