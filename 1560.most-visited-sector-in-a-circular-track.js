5574923715ff468b059a11c165d14f3c3fb16f99
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-26 17:47:31 -0700

    working on mostVisited

---
+++ 1560.most-visited-sector-in-a-circular-track.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=1560 lang=javascript
+ *
+ * [1560] Most Visited Sector in  a Circular Track
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @param {number[]} rounds
+ * @return {number[]}
+ */
+var mostVisited = function(n, rounds) {
+    const result = [];
+    const start = rounds[0];
+    const end = rounds[rounds.length - 1];
+};
+// @lc code=end
