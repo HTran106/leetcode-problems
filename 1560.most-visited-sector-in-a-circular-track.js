7404ce66be01525e08bd04acdf15bb609ba2793a
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-26 17:47:51 -0700

    done with mostVisited

--- 1560.most-visited-sector-in-a-circular-track.js
+++ 1560.most-visited-sector-in-a-circular-track.js
@@ -14,5 +14,20 @@ var mostVisited = function(n, rounds) {
     const result = [];
     const start = rounds[0];
     const end = rounds[rounds.length - 1];
+
+    if (start <= end) {
+        for (let i = start; i <= end; i++) {
+            result.push(i);
+        }
+    } else {
+        for (let i = 1; i <= end; i++) {
+            result.push(i);
+        }
+        for (let i = start; i <= n; i++) {
+            result.push(i);
+        }
+    }
+
+    return result;
 };
 // @lc code=end
