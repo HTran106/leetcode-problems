422788db13fa787ffdd139bd3bfb5388a28178ff
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-07-31 19:18:21 -0700

    working on modifyString

--- 1576.replace-all-s-to-avoid-consecutive-repeating-characters.js
+++ 1576.replace-all-s-to-avoid-consecutive-repeating-characters.js
@@ -12,6 +12,18 @@
 var modifyString = function(s) {
     const alphabet = 'abcdefghijklmnopqrstuvwxyz';
     let result = '';
-    
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '?') {
+            for (let j = 0; j < alphabet.length; j++) {
+                if (alphabet[j] !== result[i - 1] && alphabet[j] !== s[i + 1]) {
+                    result += alphabet[j];
+                    break;
+                }
+            }
+        } else {
+            result += s[i];
+        }
+    }
 };
 // @lc code=end
