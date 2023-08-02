40aafd4b223ece255b27bbc3c4a8e06b2490d880
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-08-01 19:24:08 -0700

    working on numSpecial

---
+++ 1582.special-positions-in-a-binary-matrix.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=1582 lang=javascript
+ *
+ * [1582] Special Positions in a Binary Matrix
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} mat
+ * @return {number}
+ */
+var numSpecial = function(mat) {
+    const rows = mat.length;
+    const cols = mat[0].length;
+    let result = 0;
+    
+};
+// @lc code=end
