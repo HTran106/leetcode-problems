08498592bf7b68c4aade48817591955e22b65439
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-08-02 19:26:30 -0700

    working on sumOddLengthSubarrays

---
+++ 1588.sum-of-all-odd-length-subarrays.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1588 lang=javascript
+ *
+ * [1588] Sum of All Odd Length Subarrays
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var sumOddLengthSubarrays = function(arr) {
+    const len = arr.length;
+    let result = 0;
+    
+};
+// @lc code=end
