6706e11d0ccbf0fa74fc99cfe7b94cd0a67cbd4b
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-08-26 17:11:13 -0700

    done getMaximumGenerate

--- 1646.get-maximum-in-generated-array.js
+++ 1646.get-maximum-in-generated-array.js
@@ -10,7 +10,18 @@
  * @return {number}
  */
 var getMaximumGenerated = function(n) {
-    const nums = [0, 1];
-    let max = 1;
+    let max = 0;
+    const arr = new Array(n + 1).fill(0);
+    for (let i = 0; i <= n; i++) {
+        if (i === 0 || i === 1) {
+            arr[i] = i;
+        } else if (i % 2 === 0) {
+            arr[i] = arr[i / 2];
+        } else {
+            arr[i] = arr[(i - 1) / 2] + arr[(i - 1) / 2 + 1];
+        }
+        max = Math.max(max, arr[i]);
+    }
+    return max;
 };
 // @lc code=end
