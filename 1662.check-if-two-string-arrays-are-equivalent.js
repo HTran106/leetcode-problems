489a705f94784f72e915f1488410bd0abf48c0dc
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-08-28 17:15:42 -0700

    done with arrayStringsAreEqual

--- 1662.check-if-two-string-arrays-are-equivalent.js
+++ 1662.check-if-two-string-arrays-are-equivalent.js
@@ -10,8 +10,10 @@
  * @param {string[]} word2
  * @return {boolean}
  */
-var arrayStringsAreEqual = function(word1, word2) {
-    
+var arrayStringsAreEqual = function (word1, word2) {
+    if (word1.join('') === word2.join('')) {
+        return true;
+    }
+    return false;
 };
 // @lc code=end
-
