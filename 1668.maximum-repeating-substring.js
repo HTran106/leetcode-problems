f1fa0dbf1d2c88fc33c65ed38b35a0013a2264ba
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-08-29 17:16:43 -0700

    start maxRepeating

---
+++ 1668.maximum-repeating-substring.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1668 lang=javascript
+ *
+ * [1668] Maximum Repeating Substring
+ */
+
+// @lc code=start
+/**
+ * @param {string} sequence
+ * @param {string} word
+ * @return {number}
+ */
+var maxRepeating = function(sequence, word) {
+    
+};
+// @lc code=end
