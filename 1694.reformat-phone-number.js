26a57022ada1df684ec2eb08a69b24e67489a8a2
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-09-01 17:22:04 -0700

    done reformatNumber

--- 1694.reformat-phone-number.js
+++ 1694.reformat-phone-number.js
@@ -9,10 +9,30 @@
  * @param {string} number
  * @return {string}
  */
-var reformatNumber = function(number) {
-    const arr = number.split('').filter(c => c !== ' ' && c !== '-');
-    const res = [];
+var reformatNumber = function (number) {
+    let res = '';
+    let temp = '';
+    for (let i = 0; i < number.length; i++) {
+        if (number[i] !== ' ' && number[i] !== '-') {
+            temp += number[i];
+        }
+    }
     let i = 0;
-    
+    while (i < temp.length) {
+        if (temp.length - i === 4) {
+            res += temp.slice(i, i + 2) + '-' + temp.slice(i + 2, i + 4);
+            break;
+        }
+        if (temp.length - i === 2) {
+            res += temp.slice(i, i + 2);
+            break;
+        }
+        res += temp.slice(i, i + 3) + '-';
+        i += 3;
+    }
+    if (res[res.length - 1] === '-') {
+        res = res.slice(0, res.length - 1);
+    }
+    return res;
 };
 // @lc code=end
