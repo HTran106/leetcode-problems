37332dcec34044d30738f00c78021b4b5e5e7990
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-09-01 17:20:16 -0700

    start reformatNumber

---
+++ 1694.reformat-phone-number.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=1694 lang=javascript
+ *
+ * [1694] Reformat Phone Number
+ */
+
+// @lc code=start
+/**
+ * @param {string} number
+ * @return {string}
+ */
+var reformatNumber = function(number) {
+    const arr = number.split('').filter(c => c !== ' ' && c !== '-');
+    const res = [];
+    let i = 0;
+    
+};
+// @lc code=end
