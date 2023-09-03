7ff92213019527f3417f40a5e86252bb2ef16aeb
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-09-02 17:23:58 -0700

    done maximumUnits

--- 1710.maximum-units-on-a-truck.js
+++ 1710.maximum-units-on-a-truck.js
@@ -12,5 +12,16 @@
  */
 var maximumUnits = function(boxTypes, truckSize) {
     const sorted = boxTypes.sort((a, b) => b[1] - a[1]);
+    let res = 0;
+    for (const box of sorted) {
+        if (truckSize >= box[0]) {
+            res += box[0] * box[1];
+            truckSize -= box[0];
+        } else {
+            res += truckSize * box[1];
+            break;
+        }
+    }
+    return res;
 };
 // @lc code=end
