c1271cf645587f11fa9316f01400ab476cb332b5
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-09-02 17:23:37 -0700

    start maximumUnits

---
+++ 1710.maximum-units-on-a-truck.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1710 lang=javascript
+ *
+ * [1710] Maximum Units on a Truck
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} boxTypes
+ * @param {number} truckSize
+ * @return {number}
+ */
+var maximumUnits = function(boxTypes, truckSize) {
+    const sorted = boxTypes.sort((a, b) => b[1] - a[1]);
+};
+// @lc code=end
