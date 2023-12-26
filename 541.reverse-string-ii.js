d3629f427329cd9136c4b05de5c2578aef7db384
HTran106 <90004686+HTran106@users.noreply.github.com>
2023-12-26 10:15:11 -0800

    working on reverse string 2

---
+++ 541.reverse-string-ii.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=541 lang=javascript
+ *
+ * [541] Reverse String II
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var reverseStr = function(s, k) {
+    let res = ''
+    let i = 0
+    while (i < s.length) {
+        let first = s.slice(0, k + 1)
+    }
+};
+// @lc code=end
